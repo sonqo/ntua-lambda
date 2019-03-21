24562342f2f12dd549084c809df85e7e2e22f410
Sonqo <strats_karyp@hotmail.com>
2019-03-21 23:02:58 +0200

    Proglang 1st Assignment - Colors.c

--- progLang/firstAssignment/colors.c
+++ progLang/firstAssignment/colors.c
@@ -2,28 +2,27 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-    
-    int N, M;
 
+    int i, N, M;
+
+    // Check if file is valid
     FILE *fp = fopen("Colors.txt", "r");
     if (fp == NULL){
         printf("File can not be opened\n");
         return 0;
     }
+
     // Reading length of ribbon(N) and number of colors(M)
     fscanf(fp, "%d", &N);
     fscanf(fp, "%d", &M);
 
+    // Filling array - ribbon
+    int array[N];
+    for (i = 0; i < N; i++){
+        fscanf(fp, "%d", &array[i]);
+    }
+    fclose(fp);
+
     
 
-
-
-
-
-
-
-
 }
-
-
-
