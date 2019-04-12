761733fb206f77e86acb34dad484f78e3eda2f4b
Sonqo <strats_karyp@hotmail.com>
2019-04-12 20:11:19 +0300

    Second assignment Python

--- progLang/Assignments/secondAssignment/SaveTheCat/savethecat.py
+++ progLang/Assignments/secondAssignment/SaveTheCat/savethecat.py
@@ -1,6 +1,7 @@
+import sys
 from collections import deque
 
-# Class of cat(A) water(W) and elements
+# Class of cat(A) water(W) elements
 class itemSymbol:
     def __init__(self, line, column, symbol, time, pos):
         self.line = line
@@ -9,33 +10,34 @@ class itemSymbol:
         self.time = time
         self.position = pos
 
-N = 0
-M = 0
-flag = 0
-
-# Line and columns calculation
-with open("savethecat.txt") as fileobj:
-    for line in fileobj:
-        N += 1
-        for ch in line:
-            if flag is 0 and ch != '\n':
-                M += 1
-        flag = 1
-fileobj.close()
-
-# List Declaration and border creation of X's
-array = [['X' for x in range(M+2)] for y in range(N+2)]
-
+array = []
 queue = deque()
 
-# File reading and map padding
-time = 0
-with open("savethecat.txt") as fileobj:
-    for i in range(1, N+1):
-        string = fileobj.readline()
-        for j in range (1, M+1):
-            ch = string[j-1]
-            array[i][j] = ch
-            if ch is '+' or ch is '-':
-                ch = itemSymbol(i, j, ch, time, "")
-                queue.append(ch)
+# Map padding
+with open(sys.argv[1]) as fileobj:
+    for line in fileobj:
+        # Line reading and border of X's
+        array.append('X' + (line.rstrip()) + 'X')
+fileobj.close()
+
+# Lines(N) and columns(M) calculation
+N = len(array)
+M = len(array[1]) - 2
+
+# Border creation of X's
+array.insert(0, ['X' for i in range (0, M+2)])
+array.append('X' for i in range (0, M+2))
+
+# Printing map
+for i in range(0, N+2):
+        print(*array[i], sep = ' ')
+print("\n")
+
+# Enqueing water and cat elements
+time = 1
+for i in range (1, N+1):
+    for j in range (1, M+1):
+        ch = array[i][j]
+        if ch is 'W' or ch is 'A':
+            ch = itemSymbol(i, j, ch, time, "")
+            queue.append(ch)
