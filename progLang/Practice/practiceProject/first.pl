a9d5a060b78c8e07a1f9ec5e10a3b018557db407
Sonqo <strats_karyp@hotmail.com>
2019-04-16 20:15:06 +0300

    Prolog practice

--- progLang/Practice/practiceProject/first.pl
+++ progLang/Practice/practiceProject/first.pl
@@ -1,11 +1,26 @@
-boy(rami).
-boy(hani).
+% The Wolf, Goat and Cabbage problem
 
-girl(dania).
+change(e, w).
+change(w, e).
 
-cat(lima).
+move([X, X, Goat, Cabbage], wolf, [Y ,Y, Goat, Cabbage]) :-
+    change(X, Y).
+   move([X, Wolf, X, Cabbage], goat, [Y, Wolf, Y, Cabbage]) :-
+    change(X, Y).
+   move([X, Wolf, Goat, X], cabbage, [Y, Wolf, Goat, Y]) :-
+    change(X, Y).
+   move([X, Wolf, Goat, Cab], nothing, [Y, Wolf, Goat, Cab]) :-
+    change(X, Y).
 
-person(X) :- boy(X).
-person(X) :- girl(X).
+guarded_or_separated(X, X, X).
+guarded_or_separated(_, Y, Z) :- Y \= Z.
 
-friend(W,Z) :- person(W), person(Z), W\==Z.
+safe([Man, Wolf, Goat, Cabbage]) :- 
+    guarded_or_separated(Man, Wolf, Goat), 
+    guarded_or_separated(Man, Goat, Cabbage).
+
+solution([e,e,e,e], []).
+solution(Config, [Move|Moves]) :-
+ move(Config, Move, NextConfig),
+ safe(NextConfig),
+ solution(NextConfig, Moves).
