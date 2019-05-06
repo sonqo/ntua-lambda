42a25cf3fd202b77dccc5325047f8034dbb3d9ab
Sonqo <strats_karyp@hotmail.com>
2019-05-06 10:21:20 +0300

    Colors.pl input reading

--- progLang/Assignments/secondAssignment/Colors/colors.pl
+++ progLang/Assignments/secondAssignment/Colors/colors.pl
@@ -1,18 +1,9 @@
-/*
- * A predicate that reads the input from File and returns it in
- * the last three arguments: N, K and C.
- * Example:
- *
- * ?- read_input('c1.txt', N, K, C).
- * N = 10,
- * K = 3,
- * C = [1, 3, 1, 3, 1, 3, 3, 2, 2|...].
- */
+/* A predicate that reads the input from File and returns it in the last three arguments: N, M and Ribbon. */
 
-read_input(File, N, K, C) :-
+read_input(File, N, M, Ribbon) :-
     open(File, read, Stream),
     read_line(Stream, [N, K]),
-    read_line(Stream, C).
+    read_line(Stream, Ribbon).
 
 read_line(Stream, L) :-
     read_line_to_codes(Stream, Line),
