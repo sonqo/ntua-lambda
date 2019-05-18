9a940a5bee5e911cd8197f226f48222fd3f2da84
Sonqo <strats_karyp@hotmail.com>
2019-05-18 23:04:09 +0300

    Knowledge base practice exercises

--- progLang/Practice/practiceProject/KnowledgeBase.pl
+++ progLang/Practice/practiceProject/KnowledgeBase.pl
@@ -22,8 +22,41 @@ child(bridget,caroline).
 child(caroline,donna). 
 child(donna,emily).
 descend(X,Y) :- child(X,Y). 
-descend(X,Y) :- child(X,Z), 
+descend(X,Y) :- 
+    child(X,Z), 
     descend(Z,Y).
 
 numeral(0). 
 numeral(succ(X)) :- numeral(X).
+
+directly(katarina,olga).
+directly(olga, natasha).
+directly(natasha, irina).
+in(X,Y) :- directly(X,Y).
+in(X,Y) :- 
+    directly(X,Z), 
+    in(Z,Y).
+
+connected(1,2). 
+connected(3,4). 
+connected(5,6). 
+connected(7,8). 
+connected(9,10). 
+connected(12,13). 
+connected(13,14). 
+connected(15,16). 
+connected(17,18). 
+connected(19,20). 
+connected(4,1). 
+connected(6,3). 
+connected(4,7). 
+connected(6,11). 
+connected(14,9). 
+connected(11,15). 
+connected(16,12). 
+connected(14,17). 
+connected(16,19).
+path(X,Y) :- connected(X,Y); connected(Y,X).
+path(X,Y) :-
+    connected(X,Z),
+    path(Z,Y).
