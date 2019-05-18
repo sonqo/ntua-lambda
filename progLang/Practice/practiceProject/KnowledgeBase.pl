ba582687aa30869ddc7d52ad590e859381613872
Sonqo <strats_karyp@hotmail.com>
2019-05-18 19:25:45 +0300

    Knowledge base practice exercises

--- progLang/Practice/practiceProject/KnowledgeBase.pl
+++ progLang/Practice/practiceProject/KnowledgeBase.pl
@@ -7,12 +7,23 @@ playsAirGuitar(mia):-
 playsAirGuitar(yolanda):- 
     listens2Music(yolanda). 
 
-is_digesting(X,Y)  :-  just_ate(X,Y). 
-is_digesting(X,Y)  :- 
+is_digesting(X,Y) :- just_ate(X,Y). 
+is_digesting(X,Y) :- 
     just_ate(X,Z), 
     is_digesting(Z,Y). 
-    
 just_ate(mosquito,blood(john)). 
 just_ate(frog,mosquito). 
 just_ate(stork,frog).
 
+child(bridget,caroline). 
+child(caroline,donna).
+child(anne,bridget). 
+child(bridget,caroline). 
+child(caroline,donna). 
+child(donna,emily).
+descend(X,Y) :- child(X,Y). 
+descend(X,Y) :- child(X,Z), 
+    descend(Z,Y).
+
+numeral(0). 
+numeral(succ(X)) :- numeral(X).
