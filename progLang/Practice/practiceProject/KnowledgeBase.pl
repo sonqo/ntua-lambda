9e4ab05033b939ff61973b8813bf311e01b2319f
Sonqo <strats_karyp@hotmail.com>
2019-05-18 18:48:49 +0300

    Knowledge base practice exercises

--- progLang/Practice/practiceProject/KnowledgeBase.pl
+++ progLang/Practice/practiceProject/KnowledgeBase.pl
@@ -6,3 +6,13 @@ playsAirGuitar(mia):-
     listens2Music(mia). 
 playsAirGuitar(yolanda):- 
     listens2Music(yolanda). 
+
+is_digesting(X,Y)  :-  just_ate(X,Y). 
+is_digesting(X,Y)  :- 
+    just_ate(X,Z), 
+    is_digesting(Z,Y). 
+    
+just_ate(mosquito,blood(john)). 
+just_ate(frog,mosquito). 
+just_ate(stork,frog).
+
