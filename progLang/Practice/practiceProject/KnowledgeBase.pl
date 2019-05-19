62502c1e008a49e3928249272276ecc8183311c9
Sonqo <strats_karyp@hotmail.com>
2019-05-19 17:06:52 +0300

    Prolog practice

--- progLang/Practice/practiceProject/KnowledgeBase.pl
+++ progLang/Practice/practiceProject/KnowledgeBase.pl
@@ -37,26 +37,5 @@ in(X,Y) :-
     directly(X,Z), 
     in(Z,Y).
 
-connected(1,2). 
-connected(3,4). 
-connected(5,6). 
-connected(7,8). 
-connected(9,10). 
-connected(12,13). 
-connected(13,14). 
-connected(15,16). 
-connected(17,18). 
-connected(19,20). 
-connected(4,1). 
-connected(6,3). 
-connected(4,7). 
-connected(6,11). 
-connected(14,9). 
-connected(11,15). 
-connected(16,12). 
-connected(14,17). 
-connected(16,19).
-path(X,Y) :- connected(X,Y); connected(Y,X).
-path(X,Y) :-
-    connected(X,Z),
-    path(Z,Y).
+a2b([],[]).
+a2b([a|Ta],[b|Tb]) :- a2b(Ta,Tb).
