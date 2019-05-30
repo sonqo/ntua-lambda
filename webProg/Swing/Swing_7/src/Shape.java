81827e6b5bedef321c9a416af0e0bbc099f33339
Sonqo <strats_karyp@hotmail.com>
2019-05-30 10:17:46 +0300

    Swing_7 practice

---
+++ webProg/Swing/Swing_7/src/Shape.java
@@ -0,0 +1,62 @@
+import java.awt.*;
+import static java.awt.Color.*;
+
+public class Shape extends Thread {
+
+    int r, x, y;
+    Canvas canvas;
+    int interval = 1000;
+
+    boolean red = false, blue = true; // Default color
+
+    Color color = Color.blue;
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+//    public void setColor(){
+//        if (red){
+//            blue = true;
+//            red = false;
+//            color = Color.blue;
+//        }
+//        else{
+//            blue = false;
+//            red = true;
+//            color = Color.red;
+//        }
+//    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
+    public void setRad(int r) {
+        this.r = r;
+    }
+
+    public Shape(Canvas c){
+        super();
+        canvas = c;
+        setDaemon(true);
+    }
+
+    public void run(){
+        while(true){
+            try {
+                sleep(interval);
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+//            this.setColor(); // Alternating colors of shapes
+            if (this.r == 15){ // Alternating rad of shapes
+                this.r += 10;
+            }
+            else if(this.r == 25){
+                this.r -= 10;
+            }
+            canvas.repaint();
+        }
+    }
+}
