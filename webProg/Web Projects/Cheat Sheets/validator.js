569c254471a74ddc55ab4afb5db782152191b493
Sonqo <strats_karyp@hotmail.com>
2019-06-17 16:52:57 +0300

    Javascript validation function

---
+++ webProg/Web Projects/Cheat Sheets/validator.js
@@ -0,0 +1,25 @@
+function validateFields() {
+
+    var firstNum = document.getElementById("firstNum").value;
+    var secondNum = document.getElementById("secondNum").value;
+    var symbol = document.getElementById("symbol").value;
+
+    // Validation checking
+    if (firstNum == null || firstNum == "" || secondNum == null || secondNum == "" || symbol == null || symbol == ""){
+        alert("At least one of the form fields is empty! Please try again.");
+    }
+    else{
+        if (isNaN(firstNum) || isNaN(secondNum) == true){
+            alert("Not a number! Please try again.")
+        }
+        else{
+            if (symbol !== "+" && symbol !== "-" && symbol !== "*" && symbol !== "/"){
+                alert("Please insert valid symbol!")
+            }
+        }
+    }
+}   
+    
+    
+    
+    
