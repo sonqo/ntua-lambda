ba941c877dac5109d3c1ecaef1f8f03b38545ebf
Sonqo <strats_karyp@hotmail.com>
2019-07-04 13:28:27 +0300

    Folder rename

--- progLang/Practice/Exam.pl
+++
@@ -1,6 +0,0 @@
-middle([], []).
-middle([X], [X]).
-middle([X, Y], [X, Y]).
-middle([First|Rest], [First, Middle, Last]) :-
-    append(Inside, [Last], Rest),
-    middle(Inside, Middle).
--- progLang/Practice/readFile.sml
+++
@@ -1,8 +0,0 @@
-fun parse file =
-let
-    fun next_String input = (TextIO.inputAll input) 
-    val stream = TextIO.openIn file
-    val a = next_String stream
-in
-    a
-end;
