7039921c9a878bf1f92d4dcad0a653263a9c8bb0
Sonqo <strats_karyp@hotmail.com>
2019-07-11 14:01:01 +0300

    Lin = Rin case covered optimally

--- Programming_Languages/Assignments/3/Ztalloc/ztalloc.py
+++ Programming_Languages/Assignments/3/Ztalloc/ztalloc.py
@@ -4,10 +4,7 @@ from collections import deque
 class Borders:
     """ Class of cases, containing lower border, higher border, path of numeric calculations followed to this state and number of state """
     def __init__(self, low_b, high_b, path, state):
-        self.low_b = low_b
-        self.high_b = high_b
-        self.path = path
-        self.state = state
+        self.low_b = low_b; self.high_b = high_b; self.path = path; self.state = state
 
 array = []
 queue = deque()
@@ -20,8 +17,12 @@ N = int(array[0][0]) # Number of total cases
     
 for i in range(1, N+1):
 
+    visited = [0] * 1000000
+
     x = Borders(int(array[i][0]), int(array[i][1]), "", 0) # Append one case at a time
     queue.append(x)
+
+    visited[int(array[i][1])] = 1
 
     flag = 0
 
@@ -41,27 +42,35 @@ for i in range(1, N+1):
                     if temp.path == "":
                         print("EMPTY")
                     else:
-                        print(temp.low_b, temp.high_b, temp.path, temp.state)
+                        print(temp.path, temp.state)
 
                 if (temp.low_b != 0 or temp.high_b != 0) and flag != 1:
 
                     temp_1 = temp.low_b // 2 
                     temp_2 = temp.high_b // 2 
 
-                    # temp_state += 1
+                    if visited[temp_1] != 1:
 
-                    x = Borders(temp_1, temp_2, temp.path + "h", temp.state+1)
-                    queue.append(x)
+                        temp_state += 1
+
+                        x = Borders(temp_1, temp_2, temp.path + "h", temp_state)
+                        queue.append(x)
+
+                        visited[temp_1] = 1
 
                 temp_1 = temp.low_b * 3 + 1
                 temp_2 = temp.high_b * 3 + 1
 
                 if (temp_1 <= 999999 and temp_2 <= 999999) and flag != 1:
                     
-                    # temp_state += 1
+                    if visited[temp_1] != 1:
 
-                    x = Borders(temp_1, temp_2, temp.path + "t", temp.state+1)
-                    queue.append(x)
+                        temp_state += 1
+
+                        x = Borders(temp_1, temp_2, temp.path + "t", temp_state)
+                        queue.append(x)
+
+                        visited[temp_1] = 1 
             
             else:
                 
