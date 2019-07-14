1e7e27136fd06a9303385ea8b620677cc94925d2
Sonqo <strats_karyp@hotmail.com>
2019-07-14 13:31:57 +0300

    Floodfill algorithm added

--- Programming_Languages/Assignments/3/SaveTheCat/src/Floodfill.java
+++ Programming_Languages/Assignments/3/SaveTheCat/src/Floodfill.java
@@ -14,6 +14,10 @@ public class Floodfill {
     public ItemSymbol element;
     public Queue<ItemSymbol> symbolQ = new LinkedList<ItemSymbol>();
 
+    public int start_line, start_column; // Starting coordinates
+    public int leastl, leastc; // Arjumand not in danger
+    public int lpath = 1002, cpath = 1002; // Arjumand in danger
+
     public Floodfill(String input){
         this.input = input;
         mapPadding();
@@ -31,8 +35,14 @@ public class Floodfill {
                 }
                 for (int i=1; i<=this.M; i++){
                     map[N+1][i]=line.charAt(i-1);
-                    if (line.charAt(i-1) == 'A'){
+                    if (line.charAt(i-1) == 'A'){ // Adding Arjumand
                         element = new ItemSymbol(N+1, i, "A", 0, "");
+                        symbolQ.add(element);
+                        leastl = N+1; leastc = i;
+                        start_line = N+1; start_column = i; // Keeping starting coordinates of Arjumand
+                    }
+                    if (line.charAt(i-1) == 'W'){ // Adding water elements
+                        element = new ItemSymbol(N+1, i, "W", 0, "");
                         symbolQ.add(element);
                     }
                 }
@@ -47,13 +57,6 @@ public class Floodfill {
                 map[i][0]='X';
                 map[i][this.M+1]='X';
             }
-            for (int i=0; i<=this.N+2; i++){
-                for (int j=0; j<=this.M+2; j++){
-                    System.out.print(map[i][j]);
-                }
-                System.out.println();
-            }
-
         }
         catch(FileNotFoundException ex){
             return;
@@ -64,6 +67,175 @@ public class Floodfill {
     }
 
     public void endGame(){
+        int time = 0;
+        int arjumand = 0;
+        int global_time = 0;
+        while(symbolQ.peek() != null){
+            while (global_time == time){
+                ItemSymbol element = new ItemSymbol();
+                element = ((LinkedList<ItemSymbol>) symbolQ).getFirst();
+                time = element.getTime();
+                String symbol = element.getSymbol();
+                int line = element.getLine(); int column = element.getColumn();
+                char itemWest = map[line][column-1]; char itemEast = map[line][column+1];
+                char itemNorth = map[line-1][column]; char itemSouth = map[line+1][column];
+                if (symbol.equals("A")) {
+                    if ((itemNorth != 'A') && (itemNorth != 'X') && (itemNorth != 'W')) {
+                        map[line-1][column] = 'A';
+                        ItemSymbol item = new ItemSymbol(line-1, column, "A", time+1);
+                        symbolQ.add(item);
+                        if (line-1 < leastl){
+                            leastl = line-1; leastc = column;
+                        }
+                        else if (line-1 == leastl){
+                            if (column < leastc){
+                                leastl = line-1; leastc = column;
+                            }
+                        }
+                    }
+                    if ((itemSouth != 'A') && (itemSouth != 'X') && (itemSouth != 'W')) {
+                        map[line+1][column] = 'A';
+                        ItemSymbol item = new ItemSymbol(line+1, column, "A", time+1);
+                        symbolQ.add(item);
+                        if (line+1 < leastl){
+                            leastl = line+1; leastc = column;
+                        }
+                        else if (line+1 == leastl){
+                            if (column < leastc){
+                                leastl = line+1; leastc = column;
+                            }
+                        }
+                    }
+                    if ((itemEast != 'A') && (itemEast != 'X') && (itemEast != 'W')) {
+                        map[line][column+1] = 'A';
+                        ItemSymbol item = new ItemSymbol(line, column+1, "A", time+1);
+                        symbolQ.add(item);
+                        if (line < leastl){
+                            leastl = line; leastc = column+1;
+                        }
+                        else if (line == leastl){
+                            if (column+1 < leastc){
+                                leastl = line; leastc = column+1;
+                            }
+                        }
+                    }
+                    if ((itemWest != 'A') && (itemWest != 'X') && (itemWest != 'W')) {
+                        map[line][column-1] = 'A';
+                        ItemSymbol item = new ItemSymbol(line, column-1, "A", time+1);
+                        symbolQ.add(item);
+                        if (line < leastl){
+                            leastl = line; leastc = column-1;
+                        }
+                        else if (line == leastl){
+                            if (column-1 < leastc){
+                                leastl = line; leastc = column-1;
+                            }
+                        }
+                    }
+                }
+                if (symbol.equals("W")) {
+                    if ((itemNorth != 'W') && (itemNorth != 'X')) {
+                        map[line-1][column] = 'W';
+                        ItemSymbol item = new ItemSymbol(line-1, column, "W", time+1);
+                        symbolQ.add(item);
+                        if (itemNorth == 'A'){
+                            if (global_time == arjumand){
+                                if (line-1 < lpath){
+                                    lpath = line-1; cpath = column;
+                                }
+                                else if (line-1 == lpath){
+                                    if (column < cpath){
+                                        lpath = line-1; cpath = column;
+                                    }
+                                }
+                            }
+                            else if (global_time > arjumand){
+                                arjumand = global_time;
+                                lpath = line-1; cpath = column;
+                            }
+                        }
+                    }
+                    if ((itemSouth != 'W') && (itemSouth != 'X')) {
+                        map[line+1][column] = 'W';
+                        ItemSymbol item = new ItemSymbol(line+1, column, "W", time+1);
+                        symbolQ.add(item);
+                        if (itemSouth == 'A'){
+                            if (global_time == arjumand){
+                                if (line+1 < lpath){
+                                    lpath = line+1; cpath = column;
+                                }
+                                else if (line+1 == lpath){
+                                    if (column < cpath){
+                                        lpath = line+1; cpath = column;
+                                    }
+                                }
+                            }
+                            else if (global_time > arjumand){
+                                arjumand = global_time;
+                                lpath = line+1; cpath = column;
+                            }
+                        }
+                    }
+
+                    if ((itemEast != 'W') && (itemEast != 'X')) {
+                        map[line][column+1] = 'W';
+                        ItemSymbol item = new ItemSymbol(line, column+1, "W", time+1);
+                        symbolQ.add(item);
+                        if (itemEast == 'A'){
+                            if (global_time == arjumand){
+                                if (line < lpath){
+                                    lpath = line; cpath = column+1;
+                                }
+                                else if (line == lpath){
+                                    if (column+1 < cpath){
+                                        lpath = line; cpath = column+1;
+                                    }
+                                }
+                            }
+                            else if (global_time > arjumand){
+                                arjumand = global_time;
+                                lpath = line; cpath = column+1;
+                            }
+                        }
+                    }
+
+                    if ((itemWest != 'W') && (itemWest != 'X')) {
+                        map[line][column-1] = 'W';
+                        ItemSymbol item = new ItemSymbol(line, column-1, "W", time+1);
+                        symbolQ.add(item);
+                        if (itemWest == 'A'){
+                            if (global_time == arjumand){
+                                if (line < lpath){
+                                    lpath = line; cpath = column-1;
+                                }
+                                else if (line == lpath){
+                                    if (column-1 < cpath){
+                                        lpath = line; cpath = column-1;
+                                    }
+                                }
+                            }
+                            else if (global_time > arjumand){
+                                arjumand = global_time;
+                                lpath = line; cpath = column-1;
+                            }
+                        }
+                    }
+                }
+                symbolQ.remove();
+                if (symbolQ.peek() != null){
+                    element = ((LinkedList<ItemSymbol>) symbolQ).getFirst();
+                    time = element.getTime();
+                }
+                else{
+                    global_time = -1;
+                }
+            }
+            global_time ++;
+        }
+        spurenJagd();
+    }
+
+    public void spurenJagd(){
 
     }
 
--- Programming_Languages/Assignments/3/SaveTheCat/src/ItemSymbol.java
+++ Programming_Languages/Assignments/3/SaveTheCat/src/ItemSymbol.java
@@ -3,11 +3,42 @@ public class ItemSymbol{
     public int line, column, time;
     public String symbol, position;
 
+    public ItemSymbol(){
+
+    }
+
+    public ItemSymbol(int line, int column, String symbol, int time){
+
+        this.line = line; this.column = column; this.time = time;
+        this.symbol = symbol;
+
+    }
+
     public ItemSymbol(int line, int column, String symbol, int time, String position){
 
         this.line = line; this.column = column; this.time = time;
         this.symbol = symbol; this.position = position;
 
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public String getPosition(){
+        return position;
     }
 
 }
