353d2b250a0f461021fe1bbb943afcc7e114f228
Sonqo <strats_karyp@hotmail.com>
2019-07-14 10:28:06 +0300

    File reading

--- Programming_Languages/Assignments/3/SaveTheCat/src/Arjumand.java
+++ Programming_Languages/Assignments/3/SaveTheCat/src/Arjumand.java
@@ -1,55 +1,9 @@
-import java.io.*;
-import java.util.Queue;
-import java.util.LinkedList;
-
 public class Arjumand {
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) {
 
-        BufferedReader fileInput = new BufferedReader(new FileReader("savethecat.txt"));
-        String text = fileInput.readLine();
+        Floodfill prayForArj = new Floodfill(args[0]);
 
-        int N = 0; // Calculating N-M dimensions of map
-        int M = text.length();
-
-        while (text != null) {
-            text = fileInput.readLine();
-            N++;
-        }
-
-        Queue<ItemSymbol> symbolQ = new LinkedList<ItemSymbol>();
-
-        String[][] chMap = new String[N + 2][M + 2]; // Reading Map
-
-        BufferedReader br = new BufferedReader(new FileReader("savethecat.txt"));
-        int time = 0;
-        for (int i = 1; i < N + 1; i++) {
-            text = br.readLine();
-            for (int j = 1; j < M + 1; j++) {
-                chMap[i][j] = String.valueOf(text.charAt(j - 1));
-                if (chMap[i][j].equals("A")) {
-                    ItemSymbol element = new ItemSymbol(i, j, "A", time, "");
-                    symbolQ.add(element);
-                }
-            }
-        }
-
-        for (int i = 0; i < M + 2; i++) { //Border Creation of X's
-            chMap[0][i] = "X";
-            chMap[N + 1][i] = "X";
-        }
-        for (int i = 0; i < N + 2; i++) {
-            chMap[i][0] = "X";
-            chMap[i][M + 1] = "X";
-        }
-
-
-
-        for (int i = 0; i < N + 2; i++) {
-            for (int j = 0; j < M + 2; j++) {
-                System.out.print(chMap[i][j]);
-            }
-            System.out.println();
-        }
     }
+
 }
--- Programming_Languages/Assignments/3/SaveTheCat/src/Floodfill.java
+++ Programming_Languages/Assignments/3/SaveTheCat/src/Floodfill.java
@@ -1,5 +1,70 @@
+import java.util.Queue;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+
 public class Floodfill {
 
+    public char[][] map = new char [1002][1002];
+    public String input;
+    public int N, M;
 
+    public ItemSymbol element;
+    public Queue<ItemSymbol> symbolQ = new LinkedList<ItemSymbol>();
+
+    public Floodfill(String input){
+        this.input = input;
+        mapPadding();
+        endGame();
+    }
+
+    public void mapPadding(){ // File reading - https://bit.ly/2utVcbH
+        String line;
+        try{
+            FileReader fileReader = new FileReader(input);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while((line = bufferedReader.readLine()) != null){
+                if (N==0){
+                    this.M=line.length();
+                }
+                for (int i=1; i<=this.M; i++){
+                    map[N+1][i]=line.charAt(i-1);
+                    if (line.charAt(i-1) == 'A'){
+                        element = new ItemSymbol(N+1, i, "A", 0, "");
+                        symbolQ.add(element);
+                    }
+                }
+                N++;
+            }
+            bufferedReader.close();
+            for (int i=0; i<=this.M+1; i++){ // Border creation of X's
+                this.map[0][i]='X';
+                this.map[this.N+1][i]='X';
+            }
+            for (int i=0; i<=this.N+1; i++){
+                map[i][0]='X';
+                map[i][this.M+1]='X';
+            }
+            for (int i=0; i<=this.N+2; i++){
+                for (int j=0; j<=this.M+2; j++){
+                    System.out.print(map[i][j]);
+                }
+                System.out.println();
+            }
+
+        }
+        catch(FileNotFoundException ex){
+            return;
+        }
+        catch(IOException ex){
+            return;
+        }
+    }
+
+    public void endGame(){
+
+    }
 
 }
