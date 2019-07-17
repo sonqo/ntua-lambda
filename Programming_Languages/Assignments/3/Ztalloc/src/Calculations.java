a5ec9c2b4ccfb6ef2dd631b60c6ef6accb476742
Sonqo <strats_karyp@hotmail.com>
2019-07-17 16:40:42 +0300

    Ztalloc.java

---
+++ Programming_Languages/Assignments/3/Ztalloc/src/Calculations.java
@@ -0,0 +1,21 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Calculations {
+
+    public String input;
+
+    public Calculations(String input){
+
+        this.input = input;
+        fileReading();
+
+    }
+
+    public void fileReading(){
+
+
+
+    }
+
+}
--- Programming_Languages/Assignments/3/Ztalloc/src/Ztalloc.java
+++ Programming_Languages/Assignments/3/Ztalloc/src/Ztalloc.java
@@ -2,7 +2,7 @@ public class Ztalloc {
 
     public static void main(String args[]){
 
-
+        Calculations crazyMach = new Calculations(args[0]);
 
     }
 
