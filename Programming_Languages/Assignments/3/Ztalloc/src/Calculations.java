b93182a8f30fec29bf97b9a9806d3eb57ba1ffb5
Sonqo <strats_karyp@hotmail.com>
2019-07-19 21:01:46 +0300

    File reading added

--- Programming_Languages/Assignments/3/Ztalloc/src/Calculations.java
+++ Programming_Languages/Assignments/3/Ztalloc/src/Calculations.java
@@ -1,9 +1,15 @@
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 
 public class Calculations {
 
     public String input;
+
+    public int N;
+
+    public int[][] cases = new int[11][4];
 
     public Calculations(String input){
 
@@ -13,9 +19,35 @@ public class Calculations {
     }
 
     public void fileReading(){
-
-
-
+        String line;
+        int i = 0, j = 0;
+        try{
+            FileReader fileReader = new FileReader(input);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while((line = bufferedReader.readLine()) != null){
+                for (String s : line.split(" ")){
+                    if (i == 0){
+                        cases[i][0] = Integer.parseInt(s);
+                        i ++;
+                    }
+                    else{
+                        cases[i][j] = Integer.parseInt(s);
+                        j ++;
+                        if (j % 4 == 0){
+                            i ++;
+                            j = 0;
+                        }
+                    }
+                }
+            }
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex){
+            return;
+        }
+        catch(IOException ex){
+            return;
+        }
     }
 
 }
