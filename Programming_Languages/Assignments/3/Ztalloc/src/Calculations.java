a415b0c970573f914ce74b4537f7156dc3ecea88
Sonqo <strats_karyp@hotmail.com>
2019-07-21 12:39:16 +0300

    Ztalloc.java completed

--- Programming_Languages/Assignments/3/Ztalloc/src/Calculations.java
+++ Programming_Languages/Assignments/3/Ztalloc/src/Calculations.java
@@ -6,16 +6,16 @@ import java.io.FileNotFoundException;
 
 public class Calculations {
 
-    public int N;
     public String input;
     public Integer[][] cases = new Integer[10][4];
+    public int N, flag, temp_1, temp_2, temp_state;
 
-    public Queue<Case> caseQ = new LinkedList<Case>(); // Queue declaration
+    public Queue<Case> caseQ = new LinkedList<>(); // Queue declaration
 
     public Calculations(String input){
         this.input = input;
         fileReading(); // Case reading
-        caseExpansion();
+        caseExpansion(); // Calculation expansion
     }
 
     public void fileReading(){
@@ -50,21 +50,75 @@ public class Calculations {
         }
     }
 
-    public void caseExpansion(){
+    public void caseExpansion(){ // Adding multiple values for a single key - Multimap implementation - https://bit.ly/2xYEe7h
 
         for (int i=0; i<N; i++){
 
             Case element = new Case(cases[i][0], cases[i][1], "", 0);
             caseQ.add(element);
 
+            HashMap<Integer, ArrayList<Integer>> hashCases = new HashMap<>(); // Different HashMap for every case
 
+            ArrayList<Integer> myList =  new ArrayList<Integer>();
+            myList.add(cases[i][1]);
+            hashCases.put(cases[i][0], myList);
 
+            flag = 0; // Borders found flag
+            temp_state = 0; // Keeping total number of cases examined
+
+            while ((!caseQ.isEmpty()) && (flag != 1)){
+
+                Case temp;
+                temp = caseQ.remove();
+                if ((temp.low_b >= cases[i][2]) && (temp.high_b <= cases[i][3])){ // Printing when borders are met
+                    flag = 1;
+                    if (temp.path == ""){
+                        System.out.println("EMPTY");
+                    }
+                    else{
+                        System.out.println(temp.path);
+                    }
+                }
+                if (temp.state < 1000000){ // Do not examine more than 1M states
+                    temp_1 = temp.low_b / 2;
+                    temp_2 = temp.high_b / 2;
+                    myList = hashCases.get(temp_1);
+                    if (myList == null){
+                        myList = new ArrayList<>();
+                    }
+                    if (!myList.contains(temp_2)){
+                        temp_state += 1;
+                        Case border = new Case(temp_1, temp_2, temp.path + "h", temp_state);
+                        caseQ.add(border);
+                        myList.add(temp_2);
+                        hashCases.put(temp_1, myList);
+                    }
+                    temp_1 = temp.low_b * 3 + 1;
+                    temp_2 = temp.high_b * 3 + 1;
+                    if ((temp_1 <= 999999) && (temp_2 <= 999999)){ // Machine stores numbers up to 6-digits
+                        myList = hashCases.get(temp_1);
+                        if (myList == null){
+                            myList = new ArrayList<>();
+                        }
+                        if (!myList.contains(temp_2)){
+                            temp_state += 1;
+                            Case border = new Case(temp_1, temp_2, temp.path + "t", temp_state);
+                            caseQ.add(border);
+                            myList.add(temp_2);
+                            hashCases.put(temp_1, myList);
+                        }
+                    }
+                }
+            }
+            if (flag == 1){ // Empty queue when a solution was already found
+                while (!caseQ.isEmpty()){
+                    caseQ.remove();
+                }
+            }
+            if (flag == 0){ // No solution after 1M states
+                System.out.println("IMPOSSIBLE");
+            }
         }
-
-//        System.out.print(caseQ.size());
-
     }
-
-
 
 }
--- Programming_Languages/Assignments/3/Ztalloc/src/Case.java
+++ Programming_Languages/Assignments/3/Ztalloc/src/Case.java
@@ -3,13 +3,13 @@ public class Case{
     public String path;
     public int low_b, high_b, state;
 
+    public Case(){
+    }
+
     public Case(int low_b, int high_b, String path, int state){
 
         this.low_b = low_b; this.high_b = high_b; this.state = state;
         this.path = path;
 
     }
-
-
-
 }
