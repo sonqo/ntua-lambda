0c717038b6f8f0c53e8913193a3d4d405f1b1ec0
Sonqo <strats_karyp@hotmail.com>
2019-07-29 19:03:19 +0200

    TableGame completed!

--- Web_Programming/Web_Projects/Table_Game/MyScript.js
+++ Web_Programming/Web_Projects/Table_Game/MyScript.js
@@ -1,23 +1,47 @@
 var Counters = new Array(17).fill(0); 
         
-function getWinner(){    
+function getWinner(clicked_id){    
 
-    var r = Math.floor(Math.random() * 16) + 1;
-    while (Counters[r] == 1){
-        r = Math.floor(Math.random() * 16) + 1; 
-    }
+    var r = parseInt(clicked_id);
+    var temp = document.getElementById(r);
     Counters[r] = 1;
-
-    var temp = document.getElementById(r.toString());
     temp.id = "red";
+    temp.innerHTML = "X"
 
-    var r = Math.floor(Math.random() * 16) + 1;
-    while (Counters[r] == 1){
+    r = Math.floor(Math.random() * 16) + 1;
+    while (Counters[r] != 0){
         r = Math.floor(Math.random() * 16) + 1; 
     }
-    Counters[r] = 1;
+    Counters[r] = 2;
 
     temp = document.getElementById(r.toString());
     temp.id = "blue";
+    temp.innerHTML = "Y";
 
+    countWinner();
+}
+
+function countWinner(){
+
+    var count_red = 0;
+    var count_blue = 0;
+
+    for (j = 0; j < 4; j++){
+        for (i = 1; i < 5; i++){
+            if ((Counters[i+j*4] == Counters[i+j*4+4]) && (Counters[i+j*4] != 0)){
+                if (Counters[i+j*4] == 1){
+                    count_red++;
+                }
+                else{
+                    count_blue++;
+                }
+            }
+        }
+    }
+
+    var p1 = document.getElementById("player1")
+    var p2 = document.getElementById("player2")
+
+    p1.innerHTML = "Player 1: " + count_red;
+    p2.innerHTML = "Player 2: " + count_blue;
 }
