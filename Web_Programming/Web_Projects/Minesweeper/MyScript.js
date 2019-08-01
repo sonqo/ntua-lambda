9f0c944a408d58ba4852c181ec22a3ce89c0305d
Sonqo <strats_karyp@hotmail.com>
2019-08-01 16:29:53 +0300

    Minesweeper board padding completed

--- Web_Programming/Web_Projects/Minesweeper/MyScript.js
+++ Web_Programming/Web_Projects/Minesweeper/MyScript.js
@@ -1,8 +1,9 @@
+var r;
 var flag = 0;
+var Visited = new Array(65).fill(0);
+var Counters = new Array(65).fill(0);
 
 function boardPadding(){
-    var Counters = new Array(65).fill(0);
-    var Visited = new Array(65).fill(0); 
     var q = window.prompt("Enter number of mines");
     if (q > 36){
         flag = 1;
@@ -12,20 +13,43 @@ function boardPadding(){
 
     if (flag != 1){
         for (i=0; i<q; i++){
-            var r = Math.floor(Math.random() * 64) + 1;
+            r = Math.floor(Math.random() * 64) + 1;
             while (Visited[r] == 1){
                 r = Math.floor(Math.random() * 64) + 1;
             }
             document.getElementById(r.toString()).innerHTML = "&#9827;";
             Visited[r]++;
             // document.getElementById(r.toString()).style.color = "gainsboro";
-            
-            // *! Added class labels to border elements
-            // *TODO Expand if statements for every element in the board 
 
             var temp = document.getElementById(r).className;
-            console.log(temp);
             
+            if (temp == "top"){ 
+                topElements();
+            }
+            else if (temp == "bottom"){ 
+                bottomElements();
+            }
+            else if (temp == "left"){
+                leftElements();
+            }
+            else if (temp == "right"){ 
+                rigthElements();
+            }
+            else if (temp == "lbottom"){ 
+                leftBottomElement();
+            }
+            else if (temp == "rbottom"){ 
+                rightBottomElement();
+            }
+            else if (temp == "ltop"){
+                leftTopElement();
+            }
+            else if (temp == "rtop"){ 
+                rightTopElement();
+            }
+            else{
+                centreElements();
+            }
         }
         for (i=1; i<65; i++){
             if (Counters[i] != 0){
@@ -34,5 +58,152 @@ function boardPadding(){
                 }
             }
         }
+    }
+}
+
+function topElements(){
+    if (Visited[r-1] != 1){
+        Counters[r-1]++;
+    }
+    if (Visited[r+1] != 1){
+        Counters[r+1]++;
+    }
+    if (Visited[r+8] != 1){
+        Counters[r+8]++;
+    }
+    if (Visited[r+9] != 1){
+        Counters[r+9]++;
+    }
+    if (Visited[r+7] != 1){
+        Counters[r+7]++;
+    }
+}
+
+function bottomElements(){
+    if (Visited[r-1] != 1){
+        Counters[r-1]++;
+    }
+    if (Visited[r+1] != 1){
+        Counters[r+1]++;
+    }
+    if (Visited[r-8] != 1){
+        Counters[r-8]++;
+    }
+    if (Visited[r-7] != 1){
+        Counters[r-7]++;
+    }
+    if (Visited[r-9] != 1){
+        Counters[r-9]++;
+    }
+}
+
+function leftElements(){
+    if (Visited[r+1] != 1){
+        Counters[r+1]++;
+    }
+    if (Visited[r-8] != 1){
+        Counters[r-8]++;
+    }
+    if (Visited[r+8] != 1){
+        Counters[r+8]++;
+    }
+    if (Visited[r-7] != 1){
+        Counters[r-7]++;
+    }
+    if (Visited[r+9] != 1){
+        Counters[r+9]++;
+    }
+}
+
+function rigthElements(){
+    if (Visited[r-1] != 1){
+        Counters[r-1]++;
+    }
+    if (Visited[r-8] != 1){
+        Counters[r-8]++;
+    }
+    if (Visited[r+8] != 1){
+        Counters[r+8]++;
+    }
+    if (Visited[r+7] != 1){
+        Counters[r+7]++;
+    }
+    if (Visited[r-9] != 1){
+        Counters[r-9]++;
+    }
+}
+
+function leftTopElement(){
+    if (Visited[r+1] != 1){
+        Counters[r+1]++;
+    }
+    if (Visited[r+8] != 1){
+        Counters[r+8]++;
+    }
+    if (Visited[r+9] != 1){
+        Counters[r+9]++;
+    }
+}
+
+function rightTopElement(){
+    if (Visited[r-1] != 1){
+        Counters[r-1]++;
+    }
+    if (Visited[r+8] != 1){
+        Counters[r+8]++;
+    }
+    if (Visited[r+7] != 1){
+        Counters[r+7]++;
+    }
+}
+
+function leftBottomElement(){
+    if (Visited[r+1] != 1){
+        Counters[r+1]++;
+    }
+    if (Visited[r-8] != 1){
+        Counters[r-8]++;
+    }
+    if (Visited[r-7] != 1){
+        Counters[r-7]++;
+    }
+}
+
+function rightBottomElement(){
+    if (Visited[r-1] != 1){
+        Counters[r-1]++;
+    }
+    if (Visited[r-8] != 1){
+        Counters[r-8]++;
+    }
+    if (Visited[r-9] != 1){
+        Counters[r-9]++;
+    }
+}
+
+function centreElements(){
+    if (Visited[r-1] != 1){
+        Counters[r-1]++;
+    }
+    if (Visited[r+1] != 1){
+        Counters[r+1]++;
+    }
+    if (Visited[r-8] != 1){
+        Counters[r-8]++;
+    }
+    if (Visited[r+8] != 1){
+        Counters[r+8]++;
+    }
+    if (Visited[r+7] != 1){
+        Counters[r+7]++;
+    }
+    if (Visited[r+9] != 1){
+        Counters[r+9]++;
+    }
+    if (Visited[r-7] != 1){
+        Counters[r-7]++;
+    }
+    if (Visited[r-9] != 1){
+        Counters[r-9]++;
     }
 }
