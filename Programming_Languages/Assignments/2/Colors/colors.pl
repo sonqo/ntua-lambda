59adbc2ce5f59c5c0560edab6eab8dfc4ac21a1b
Sonqo <strats_karyp@hotmail.com>
2019-08-07 16:35:18 +0300

    Colors.pl updated

--- Programming_Languages/Assignments/2/Colors/colors.pl
+++ Programming_Languages/Assignments/2/Colors/colors.pl
@@ -6,10 +6,10 @@ build(X, N, [X|L]) :- N > 0, N1 is N - 1, build(X, N1, L).
 replace([_|T], 0, X, [X|T]).
 replace([H|T], I, V, [H|N]):- I > 0, I1 is I-1, replace(T, I1, V, N).
 
-% A predicate that checks whether a list has none zero elements
+% A predicate correct/1 that checks whether the counter list is not zero
 check([]).
 check([H|L]) :- \+H = 0, check(L).
+correct([H|L]) :- H = 0, check(L).
 
 update([H|_], List, NewList) :- replace(List, Head, El, NewList), nth1(H, List, Elem), El is Elem+1, Head is H-1, !.
 
-% TODO: Experiment with while looping using the update/3 and check/1 predicates above.
