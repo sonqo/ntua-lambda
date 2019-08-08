543c569a6a82ff50ab17a98c4bfed4e7123a3ed5
Sonqo <strats_karyp@hotmail.com>
2019-08-08 21:33:15 +0200

    Pointer approach

--- Programming_Languages/Assignments/2/Colors/colors.pl
+++ Programming_Languages/Assignments/2/Colors/colors.pl
@@ -1,18 +1,20 @@
-% A predicate that initializes a list of N elements to the value of X.
+% A predicate that initializes a list of N elements to the value of X
 build(_, 0, []). 
 build(X, N, [X|L]) :- N > 0, N1 is N - 1, build(X, N1, L).
 
-% A predicate update/3 that returns an update counter list for ribbon - https://bit.ly/2yKGpMf
+% A predicate update_inc/3 that increases respective counters of colors - https://bit.ly/2yKGpMf
 replace([_|T], 0, X, [X|T]).
 replace([H|T], I, V, [H|N]):- I > 0, I1 is I-1, replace(T, I1, V, N).
-update([H|_], List, NewList) :- replace(List, H, El, NewList), nth0(H, List, Elem), El is Elem+1, !.
+update_inc(List, Index, NewList) :- replace(List, Index, El, NewList), nth0(Index, List, Elem), El is Elem+1, !.
 
-% A predicate correct/1 that checks whether the counter list is not zero
+% A predicate update_inc/3 that decreases respective counters of colors
+update_dec(List, Index, NewList) :- replace(List, Index, El, NewList), nth0(Index, List, Elem), El is Elem-1, !.
+
+% A predicate all_colors/1 that checks colors counter to decide whethers all colors are met 
 check([]).
 check([H|L]) :- \+H = 0, check(L).
 all_colors([H|L]) :- H = 0, check(L).
 
-% A predicate locate_seq/3 that returns the counter list of the first completed sequence
-locate_seq(_, Counters, NewCounters) :- all_colors(Counters), NewCounters = Counters, !.
-locate_seq([H|L], Counters, FinalCounters) :- update([H|L], Counters, NewCounters), locate_seq(L, NewCounters, FinalCounters).
-
+% A predicate locate_seq/3 that returns the counter list of the first completed sequence of colors
+% locate_seq(_, Counters, NewCounters, _, _) :- all_colors(Counters), NewCounters = Counters, !.
+% locate_seq([H|L], Counters, FinalCounters, StartingP, FinalP) :- update_inc([H|L], Counters, NewCounters), locate_seq(L, NewCounters, FinalCounters).
