30e7ef87bc35dc75cd47c7cc15d28eefa21493bf
Sonqo <strats_karyp@hotmail.com>
2019-08-09 19:37:15 +0300

    Explanatory comments added

--- Programming_Languages/Assignments/2/Colors/colors.pl
+++ Programming_Languages/Assignments/2/Colors/colors.pl
@@ -8,11 +8,14 @@ build(X, N, [X|L]) :- N > 0, N1 is N - 1, build(X, N1, L), !.
 
 % A predicate update_inc/3 that increases respective counters of colors - https://bit.ly/2yKGpMf
 replace([_|T], 0, X, [X|T]).
-replace([H|T], I, V, [H|N]):- I > 0, I1 is I-1, replace(T, I1, V, N).
-update_inc(List, Index, NewList) :- replace(List, Index, El, NewList), nth0(Index, List, Elem), El is Elem+1, !.
+replace([H|T], I, V, [H|N]) :- 
+    I > 0, I1 is I-1, replace(T, I1, V, N).
+update_inc(List, Index, NewList) :- 
+    replace(List, Index, El, NewList), nth0(Index, List, Elem), El is Elem+1, !.
 
 % A predicate update_inc/3 that decreases respective counters of colors
-update_dec(List, Index, NewList) :- replace(List, Index, El, NewList), nth0(Index, List, Elem), El is Elem-1, !.
+update_dec(List, Index, NewList) :- 
+    replace(List, Index, El, NewList), nth0(Index, List, Elem), El is Elem-1, !.
 
 % A predicate all_colors/1 that checks colors counter to decide whethers all colors are met 
 check([]).
@@ -20,8 +23,10 @@ check([H|L]) :- \+H = 0, check(L).
 all_colors([H|L]) :- H = 0, check(L).
 
 % A predicate locate_tail/5 that locates the ending pointer of a valid sequence
-locate_tail(_, Counters, EndingP, GlobalIteration, FinalCounters, FinalP) :- GlobalIteration = 0, all_colors(Counters), FinalCounters = Counters, FinalP is EndingP-1, !.
-locate_tail(_, Counters, EndingP, GlobalIteration, FinalCounters, FinalP) :- \+GlobalIteration = 0, all_colors(Counters), FinalCounters = Counters, FinalP is EndingP-1, !.
+locate_tail(_, Counters, EndingP, GlobalIteration, FinalCounters, FinalP) :- % Check all counter list for only the first iteration
+    GlobalIteration = 0, all_colors(Counters), FinalCounters = Counters, FinalP is EndingP-1, !.
+locate_tail(_, Counters, EndingP, GlobalIteration, FinalCounters, FinalP) :- % Check only the respective counter cell for the rest of iterations
+    \+GlobalIteration = 0, all_colors(Counters), FinalCounters = Counters, FinalP is EndingP-1, !.
 locate_tail(List, Counters, EndingP, _, FinalCounters, FinalP) :- 
     nth0(EndingP, List, Index), update_inc(Counters, Index, NewCounters),
     NewEndingP is EndingP+1, locate_tail(List, NewCounters, NewEndingP, 1, FinalCounters, FinalP).
@@ -34,5 +39,5 @@ locate_head(List, Counters, StartingP, Iteration, FinalCounters, FinalP) :-
         false
     ).
 locate_head(List, Counters, StartingP, Iteration, FinalCounters, FinalP) :- 
-    nth0(StartingP, List, Index), update_dec(Counters, Index, NewCounters), 
-    NewStartingP is StartingP+1, NewIteration is Iteration+1, locate_head(List, NewCounters, NewStartingP, NewIteration, FinalCounters, FinalP).
+    nth0(StartingP, List, Index), update_dec(Counters, Index, NewCounters), NewStartingP is StartingP+1, 
+    NewIteration is Iteration+1, locate_head(List, NewCounters, NewStartingP, NewIteration, FinalCounters, FinalP).
