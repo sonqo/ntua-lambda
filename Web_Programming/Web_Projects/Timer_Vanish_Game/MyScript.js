43f233fd3772bf09e338a51d63f99e3ca920971e
Sonqo <strats_karyp@hotmail.com>
2019-08-29 13:20:57 +0300

    Timer game improvements

--- Web_Programming/Web_Projects/Timer_Vanish_Game/MyScript.js
+++ Web_Programming/Web_Projects/Timer_Vanish_Game/MyScript.js
@@ -1,3 +1,4 @@
+var tries = 0
 var total_tiles = 0;
 var total_seconds = 0;
 
@@ -8,6 +9,7 @@ function initialBoard(){
 
 function vanishBoard(clicked_id){
     if (document.getElementById(clicked_id).style.background == "gainsboro"){
+        tries ++
         r = Math.floor(Math.random() * 16) + 1;
         document.getElementById(r.toString()).style.background = "gainsboro";
         for (i = 1; i < 17; i++){
@@ -19,10 +21,14 @@ function vanishBoard(clicked_id){
             var timerVar = setInterval(countTimer, 1000);
         }
         total_tiles++;
+        if (tries == 10){
+            window.alert("Congrats! You got 10 tiles in " + total_seconds + " seconds");
+        }
     }
 }
 
 function countTimer() {
    ++total_seconds;
-   document.getElementById("timer").innerHTML = total_seconds;
+   document.getElementById("timer").innerHTML = "Total Time:" + total_seconds;
 }
+
