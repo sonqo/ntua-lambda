1d5a16a74b85e6c724239c763175697b38f74963
Sonqo <strats_karyp@hotmail.com>
2019-09-08 16:35:09 +0300

    Added Seaside.jave for Olympics_2019 preparation

---
+++ Programming_Languages/Olympics/Seaside/src/Seaside.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Seaside {
+
+    public static void main(String args[]) throws IOException {
+
+        Buildings calcBuildings = new Buildings();
+
+    }
+
+}
