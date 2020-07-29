ef5a23215a29b3b4a9c8f01a0e66882c431d405e
Sonqo <strats_karyp@hotmail.com>
2020-07-29 10:00:15 +0300

    Completed ontology and respective code added

--- 5_Knowledge_Ontologies/parser.py
+++ 5_Knowledge_Ontologies/parser.py
@@ -1,15 +1,16 @@
 import pandas as pd
 from unidecode import unidecode
 
-# # Parsing Agency CSV file
-agency = pd.read_csv('data/agency.txt')
-with open('trans_turtle.owl', 'a') as owl_file:
+with open('transport-ontology.owl', 'a') as owl_file:
+
+    # Parsing Agency CSV file
+    agency = pd.read_csv('data/agency.txt')
     for index, row in agency.iterrows():
         i = 0
         for e in row:
             if i == 0:
                 i += 1
-                owl_file.write('\n:' + unidecode(str(e)) + ' rdf:type ' + 'Agency ;\n')
+                owl_file.write('\n:' + unidecode(str(e).replace(' ', '').replace('/', '-')) + ' rdf:type ' + ':Agency ;\n')
             elif i == 1:
                 i += 1
                 owl_file.write(' '*8 + ':agencyURL "' + unidecode(str(e)) + '"^^xsd:string ;\n')
@@ -23,27 +24,25 @@ with open('trans_turtle.owl', 'a') as owl_file:
                 i += 1
                 owl_file.write(' '*8 + ':agencyPhone "' + unidecode(str(e)) + '"^^xsd:string .\n\n')
 
-#Parsing Calendar CSV file
-calendar = pd.read_csv('data/calendar.txt')
-with open('trans_turtle.owl', 'a') as owl_file:
+    #Parsing Calendar CSV file
+    calendar = pd.read_csv('data/calendar.txt')
     for index, row in calendar.iterrows():
         i = 0
         for e in row:
             if i == 0:
                 i += 1
-                owl_file.write(':' + unidecode(str(e)) + ' rdf:type ' + 'Service .\n\n')
+                owl_file.write(':' + unidecode(str(e).replace(' ', '').replace('/', '-')) + ' rdf:type ' + ':Service .\n\n')
             else:
                 i += 1
 
-#Parsing Routes CSV file
-route = pd.read_csv('data/routes.txt')
-with open('trans_turtle.owl', 'a') as owl_file:
+    #Parsing Routes CSV file
+    route = pd.read_csv('data/routes.txt')
     for index, row in route.iterrows():
         i = 0
         for e in row:
             if i == 0:
                 i += 1
-                owl_file.write(':' + unidecode(str(e)) + ' rdf:type ' + 'Route ;\n')
+                owl_file.write(':' + unidecode(str(e).replace(' ', '').replace('/', '-')) + ' rdf:type ' + ':Route ;\n')
             elif i == 2:
                 i += 1
                 owl_file.write(' '*8 + ':routeName "' + unidecode(str(e)) + '"^^xsd:string ;\n')
@@ -53,15 +52,14 @@ with open('trans_turtle.owl', 'a') as owl_file:
             else:
                 i += 1
 
-# Parsing Stops CSV file
-stops = pd.read_csv('data/stops.txt')
-with open('trans_turtle.owl', 'a') as owl_file:
+    # Parsing Stops CSV file
+    stops = pd.read_csv('data/stops.txt')
     for index, row in stops.iterrows():
         i = 0
         for e in row:
             if i == 0:
                 i += 1
-                owl_file.write(':' + unidecode(str(e)) + ' rdf:type ' + 'Stop ;\n')
+                owl_file.write(':' + unidecode(str(e).replace(' ', '').replace('/', '-')) + ' rdf:type ' + ':Stop ;\n')
             elif i == 2:
                 i += 1
                 owl_file.write(' '*8 + ':stopName "' + unidecode(str(e)) + '"^^xsd:string ;\n')
@@ -77,12 +75,11 @@ with open('trans_turtle.owl', 'a') as owl_file:
             else:
                 i += 1
 
-# Parsing Trips CSV file
-trips = pd.read_csv('data/trips.txt')
-with open('trans_turtle.owl', 'a') as owl_file:
+    # Parsing Trips CSV file
+    trips = pd.read_csv('data/trips.txt')
     for index, row in trips.iterrows():
         i = 0
-        owl_file.write(':' + unidecode(str(row[2])) + ' rdf:type ' + 'Trip ;\n')
+        owl_file.write(':' + unidecode(str(row[2]).replace(' ', '').replace('/', '-')) + ' rdf:type ' + ':Trip ;\n')
         for e in row:
             if i == 0:
                 i += 1
@@ -95,7 +92,6 @@ with open('trans_turtle.owl', 'a') as owl_file:
                 owl_file.write(' '*8 + ':tripHeadsign "' + unidecode(str(e)) + '"^^xsd:string ;\n')
             elif i == 4:
                 i += 1
-                owl_file.write(' '*8 + ':tripDirection ' + unidecode(str(e)) + '^^xsd:int ;\n\n')
+                owl_file.write(' '*8 + ':tripDirection "' + unidecode(str(e)) + '"^^xsd:int .\n\n')
             else:
                 i += 1
-
