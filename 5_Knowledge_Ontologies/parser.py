54daaee741f9a16549e1f29881d24989ec688e89
Sonqo <strats_karyp@hotmail.com>
2020-07-30 20:19:05 +0300

    Added objectice properties handling

--- 5_Knowledge_Ontologies/parser.py
+++ 5_Knowledge_Ontologies/parser.py
@@ -1,10 +1,16 @@
 import pandas as pd
 from unidecode import unidecode
 
+trips = pd.read_csv('data/trips.txt')
+stops = pd.read_csv('data/stops.txt')
+route = pd.read_csv('data/routes.txt')
+agency = pd.read_csv('data/agency.txt')
+stimes = pd.read_csv('data/stop_times.txt')
+calendar = pd.read_csv('data/calendar.txt')
+
 with open('transport-ontology.owl', 'a') as owl_file:
 
     # Parsing Agency CSV file
-    agency = pd.read_csv('data/agency.txt')
     for index, row in agency.iterrows():
         i = 0
         for e in row:
@@ -25,7 +31,6 @@ with open('transport-ontology.owl', 'a') as owl_file:
                 owl_file.write(' '*8 + ':agencyPhone "' + unidecode(str(e)) + '"^^xsd:string .\n\n')
 
     #Parsing Calendar CSV file
-    calendar = pd.read_csv('data/calendar.txt')
     for index, row in calendar.iterrows():
         i = 0
         for e in row:
@@ -36,7 +41,6 @@ with open('transport-ontology.owl', 'a') as owl_file:
                 i += 1
 
     #Parsing Routes CSV file
-    route = pd.read_csv('data/routes.txt')
     for index, row in route.iterrows():
         i = 0
         for e in row:
@@ -48,12 +52,12 @@ with open('transport-ontology.owl', 'a') as owl_file:
                 owl_file.write(' '*8 + ':routeName "' + unidecode(str(e)) + '"^^xsd:string ;\n')
             elif i == 5:
                 i += 1
-                owl_file.write(' '*8 + ':routeColor "' + unidecode(str(e)) + '"^^xsd:string .\n\n')
+                owl_file.write(' '*8 + ':routeColor "' + unidecode(str(e)) + '"^^xsd:string ;\n')
+                owl_file.write(' '*8 + ':doneVia :UBus .\n\n' )
             else:
                 i += 1
 
     # Parsing Stops CSV file
-    stops = pd.read_csv('data/stops.txt')
     for index, row in stops.iterrows():
         i = 0
         for e in row:
@@ -76,17 +80,16 @@ with open('transport-ontology.owl', 'a') as owl_file:
                 i += 1
 
     # Parsing Trips CSV file
-    trips = pd.read_csv('data/trips.txt')
     for index, row in trips.iterrows():
         i = 0
         owl_file.write(':' + unidecode(str(row[2]).replace(' ', '').replace('/', '-')) + ' rdf:type ' + ':Trip ;\n')
         for e in row:
             if i == 0:
                 i += 1
-                owl_file.write(' '*8 + ':routeID "' + unidecode(str(e)) + '"^^xsd:string ;\n')
+                owl_file.write(' '*8 + ':belongsTo :' + unidecode(str(e).replace(' ', '').replace('/', '-')) + ' ;\n')
             elif i == 1:
                 i += 1
-                owl_file.write(' '*8 + ':serviceID "' + unidecode(str(e)) + '"^^xsd:string ;\n')
+                owl_file.write(' '*8 + ':hasService :' + unidecode(str(e).replace(' ', '').replace('/', '-')) + ' ;\n')
             elif i == 3:
                 i += 1
                 owl_file.write(' '*8 + ':tripHeadsign "' + unidecode(str(e)) + '"^^xsd:string ;\n')
@@ -96,21 +99,23 @@ with open('transport-ontology.owl', 'a') as owl_file:
             else:
                 i += 1
 
-    # Parsing Stops CSV file
-    stimes = pd.read_csv('data/stop_times.txt')
+    # Parsing StopTime CSV file
     for index, row in stimes.iterrows():
         i = 0
-        owl_file.write(':' + unidecode(str(row[3]).replace(' ', '').replace('/', '-')) + ' rdf:type ' + ':StopTime ;\n')
+        owl_file.write(':' + unidecode(str(row[3]).replace(' ', '').replace('/', '-')) + 'ST rdf:type ' + ':StopTime ;\n')
         for e in row:
             if i == 0:
                 i += 1
-                owl_file.write(' '*8 + ':tripID "' + unidecode(str(e)) + '"^^xsd:string ;\n')
+                owl_file.write(' '*8 + ':stopFrom :' + unidecode(str(e).replace(' ', '').replace('/', '-')) + ' ;\n')
             elif i == 1:
                 i += 1
                 owl_file.write(' '*8 + ':arrivalTime "' + unidecode(str(e)) + '"^^xsd:string ;\n')
             elif i == 2:
                 i += 1
                 owl_file.write(' '*8 + ':departureTime "' + unidecode(str(e)) + '"^^xsd:string ;\n')
+            elif i == 3:
+                i += 1
+                owl_file.write(' '*8 + ':hasStop :' + unidecode(str(e).replace(' ', '').replace('/', '-')) + ' ;\n')
             elif i == 4:
                 i += 1
                 owl_file.write(' '*8 + ':stopSequence "' + unidecode(str(e)) + '"^^xsd:int .\n\n')
