8814672e31ab5574730c6a6c46917a1f2211f783
Sonqo <strats_karyp@hotmail.com>
2020-08-05 22:43:34 +0300

    Addition on JavaFX frame

--- 5_MediaLab/src/sample/Main.java
+++ 5_MediaLab/src/sample/Main.java
@@ -5,18 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("MediaLab Airport");
+        primaryStage.getIcons().add(new Image("file:/C:/Users/Sonqo/Desktop/Ntua_Lambda/5_MediaLab/images/stackoverflow_v1.png"));
+        primaryStage.setScene(new Scene(root, 1024, 624));
         primaryStage.show();
     }
-
-
+    
     public static void main(String[] args) {
         launch(args);
     }
