a5e75704bf9501e60ed01acb451cd916c19048ad
Sonqo <strats_karyp@hotmail.com>
2020-11-05 21:49:35 +0200

    Added edge struct for respective reading

--- 6_Algorithms_Complexity/1/Rick_&_Morty/main.c
+++ 6_Algorithms_Complexity/1/Rick_&_Morty/main.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
+#include <malloc.h>
+
+struct edge {
+    int base, destination, weight;
+};
 
 int main() {
 
     int NMs[2];
-    for (int i=0; i<2; i++){ // NMs[0]=N : numbers of universes | NMs[1]=M : number of portals
+    for (int i=0; i<2; i++) { // NMs[0]=N : numbers of universes | NMs[1]=M : number of portals
         scanf("%d", &NMs[i]);
     }
 
     int array[NMs[0]];
-    int portals[NMs[1]][3];
-    for (int i=0; i<NMs[0]; i++){ // read unordered array
+    for (int i=0; i<NMs[0]; i++) { // read unordered array
         scanf("%d", &array[i]);
     }
-    for (int i=0; i<NMs[1]; i++){ // read M portals and their respective widths
+
+    struct edge* portals[NMs[1]];
+    for (int i=0; i<NMs[1]; i++) { // read array of points to EDGE struct
+        int acc[3];
+        portals[i] = (struct edge*) malloc(sizeof(struct edge));
         for (int j=0; j<3; j++){
-            scanf("%d", &portals[i][j]);
+            scanf("%d", &acc[j]);
         }
+        portals[i]->base = acc[0];
+        portals[i]->destination = acc[1];
+        portals[i]->weight = acc[2];
     }
 
     return 0;
