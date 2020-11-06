bb028aadab07cfaec3ef05d90f193e586b7dc47a
Sonqo <strats_karyp@hotmail.com>
2020-11-06 09:19:28 +0200

    Added MergeSort, sorting edges according to their weights

--- 6_Algorithms_Complexity/1/Rick_&_Morty/main.c
+++ 6_Algorithms_Complexity/1/Rick_&_Morty/main.c
@@ -5,6 +5,65 @@ struct edge {
     int base, destination, weight;
 };
 
+void merge(struct edge** arr, int l, int m, int r) {
+
+    int i, j, k;
+    int n1 = m-l+1;
+    int n2 = r-m;
+
+    struct edge* L[n1];
+    struct edge* R[n2];
+
+    // Copy arrays
+    for (int i=0; i<n1; i++) {
+        L[i] = arr[l+i];
+    }
+    for (int i=0; i<n2; i++) {
+        R[i] = arr[m+1+i];
+    }
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i<n1 && j<n2) {
+        if (L[i]->weight <= R[j]->weight) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Filling unsorted items
+    while (i<n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j<n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+
+}
+
+void mergesort(struct edge** arr, int l, int r) {
+
+    if (l<r) {
+
+        int m = l + (r-l)/2;
+
+        mergesort(arr, l ,m);
+        mergesort(arr, m+1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
 int main() {
 
     int NMs[2];
@@ -20,7 +79,7 @@ int main() {
     struct edge* portals[NMs[1]];
     for (int i=0; i<NMs[1]; i++) { // read array of points to EDGE struct
         int acc[3];
-        portals[i] = (struct edge*) malloc(sizeof(struct edge));
+        portals[i] = malloc(sizeof(struct edge));
         for (int j=0; j<3; j++){
             scanf("%d", &acc[j]);
         }
@@ -28,6 +87,12 @@ int main() {
         portals[i]->destination = acc[1];
         portals[i]->weight = acc[2];
     }
+
+    mergesort(portals, 0, NMs[1]); //sort pointers of edges according to their weight
+
+//    for (int i=0; i<NMs[1]; i++){
+//        printf("%d ", portals[i]->weight);
+//    }
 
     return 0;
 }
