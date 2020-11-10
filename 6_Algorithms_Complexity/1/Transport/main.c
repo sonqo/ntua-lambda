6f78dacde139589671d635231152ef76020fee67
Sonqo <strats_karyp@hotmail.com>
2020-11-10 19:02:22 +0200

    Improved complexity, completed Transports

--- 6_Algorithms_Complexity/1/Transport/main.c
+++ 6_Algorithms_Complexity/1/Transport/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX(a,b) (((a)>(b))?(a):(b))
+typedef long long int SHIPIT;
 
 int compare(const void * a, const void * b) { // qsort comparison function
     return ( *(int*)a - *(int*)b );
 }
 
-long long int discard(long long int* array, long long int* new_array, long long int length) {
-    long long int t=-1;
-    long long int max_dist = 0;
-    for (long long int i=0; i<length; i++) {
+SHIPIT discard(SHIPIT const* array, SHIPIT* new_array, SHIPIT length) {
+    SHIPIT t=-1;
+    SHIPIT max_dist = 0;
+    for (SHIPIT i=0; i<length; i++) {
         if (array[i] != array[i+1]) {
             max_dist++;
             new_array[++t] = array[i];
@@ -19,65 +19,43 @@ long long int discard(long long int* array, long long int* new_array, long long 
     return max_dist;
 }
 
-long long int check_capacity(long long int curr_capacity, long long int* distances_nd, long long int uniq, long long int max_distance, long long int* TCs, long long int max_time) {
-    long long int curr = curr_capacity;
+SHIPIT check_capacity(SHIPIT curr_capacity, SHIPIT const* distances_nd, SHIPIT uniq, SHIPIT max_distance, SHIPIT const* TCs, SHIPIT max_time) {
+    SHIPIT curr = curr_capacity;
     if (curr_capacity >= max_distance*TCs[1]) {
         if (max_distance*TCs[2] <= max_time) {
-            long long int Xs, Xf = 0;
-            long long int Ts, Tf = 0;
-            long long int curr_distance = max_distance;
-            while (curr_capacity-TCs[3] >= (curr_distance-1)*TCs[1]) {
-                Xf++;
-                Tf += TCs[2];
-                curr_distance--;
-                curr_capacity -= TCs[3];
-            }
-            Xs = max_distance-Xf;
-            if (Xs < 0) {
-                Xs = 0;
-            }
-            Ts = Xs*TCs[0];
-            long long int total_time = Ts + Tf;
-            if (total_time <= max_time) {
-                long long int global_time = 0;
-                long long int curr_distance, initial;
-                for (long long int i=1; i<uniq; i++) {
-                    Xf = 0;
-                    Tf = 0;
-                    total_time = 0;
-                    curr_capacity = curr;
-                    initial = distances_nd[i]-distances_nd[i-1];
-                    curr_distance = distances_nd[i]-distances_nd[i-1];
-                    while ((curr_capacity-TCs[3] >= (curr_distance-1)*TCs[1]) && (curr_distance > 0)){
-                        Xf++;
-                        Tf += TCs[2];
-                        curr_distance--;
-                        curr_capacity -= TCs[3];
-                    }
-                    Xs = initial-Xf;
-                    if (Xs < 0) {
-                        Xs = 0;
-                    }
+            SHIPIT total_time;
+            SHIPIT Xs, Xf, Ts, Tf;
+            SHIPIT global_time = 0;
+            SHIPIT curr_distance, initial;
+            for (SHIPIT i=1; i<uniq; i++) {
+                total_time = 0;
+                curr_capacity = curr;
+                initial = distances_nd[i]-distances_nd[i-1];
+                curr_distance = distances_nd[i]-distances_nd[i-1];
+                Xs = (curr_capacity - curr_distance*TCs[3]) / (TCs[1] - TCs[3]); // capacity = Cs*Xs + (d-Xs)*Cf
+                if (initial < Xs) { //distance not reachable due to capacity
+                    return -1;
+                } else {
+                    Xf = curr_distance - Xs;
                     Ts = Xs*TCs[0];
+                    Tf = Xf*TCs[2];
                     total_time += Ts + Tf;
                     global_time += total_time;
                     if (global_time > max_time) {
                         return -1;
                     }
                 }
-                return 1;
-            } else {
-                return -1;
             }
-        } else { // distance not drivable in time with fast-capacity
+            return 1;
+        } else { // distance not reachable in time
             return 0;
         }
-    } else { // distance not drivable with slow-capacity
+    } else { // distance not reachable due to capacity
         return -1;
     }
 }
 
-long long int binary_search_answer(long long int lower, long long int higher, long long int* distances_nd, long long int uniq, long long int max_distance, long long int* TCs, long long int max_time) {
+SHIPIT binary_search_answer(SHIPIT lower, SHIPIT higher, SHIPIT* distances_nd, SHIPIT uniq, SHIPIT max_distance, SHIPIT* TCs, SHIPIT max_time) {
     if (lower == higher) {
         if (check_capacity(lower, distances_nd, uniq, max_distance, TCs, max_time) >= 0) {
             return higher;
@@ -85,7 +63,7 @@ long long int binary_search_answer(long long int lower, long long int higher, lo
             return -1;
         }
     } else {
-        long long int curr = (lower+higher)/2;
+        SHIPIT curr = (lower+higher)/2;
         if (check_capacity(curr, distances_nd, uniq, max_distance, TCs, max_time) > 0) {
             return binary_search_answer(lower, curr, distances_nd, uniq, max_distance, TCs, max_time);
         } else if (check_capacity(curr, distances_nd, uniq, max_distance, TCs, max_time) < 0) {
@@ -98,15 +76,15 @@ long long int binary_search_answer(long long int lower, long long int higher, lo
 
 int main() {
 
-    long long int* NKDTs = malloc(4 * sizeof(long long int)); // NKDTs0] : number of cars | NKDTs0[1] : number of stations | NKDTs[2] : distance | NKDTs[3] : time
-    for (long long int i=0; i<4; i++) {
-        scanf("%lld", &NKDTs[i]);
+    SHIPIT* NKDTs = malloc(4 * sizeof(SHIPIT)); // NKDTs0] : number of cars | NKDTs0[1] : number of stations | NKDTs[2] : distance | NKDTs[3] : time
+    for (SHIPIT i=0; i<4; i++) {
+            scanf("%lld", &NKDTs[i]);
     }
 
-    long long int max_capacity = 0;
-    long long int* cars_price = malloc(NKDTs[0] * sizeof(long long int));
-    long long int* cars_capacity = malloc(NKDTs[0] * sizeof(long long int));
-    for (long long int i=0; i<NKDTs[0]; i++) {
+    SHIPIT max_capacity = 0;
+    SHIPIT* cars_price = malloc(NKDTs[0] * sizeof(SHIPIT));
+    SHIPIT* cars_capacity = malloc(NKDTs[0] * sizeof(SHIPIT));
+    for (SHIPIT i=0; i<NKDTs[0]; i++) {
         scanf("%lld", &cars_price[i]); // read price
         scanf("%lld", &cars_capacity[i]); // read capacity
         if (cars_capacity[i] > max_capacity) { // get largest possible capacity
@@ -114,39 +92,39 @@ int main() {
         }
     }
 
-    long long int* distances = malloc((NKDTs[1]+2) * sizeof(long long int));
-    long long int* distances_nd = malloc((NKDTs[1]+2) * sizeof(long long int));
-    for (long long int i=1; i<NKDTs[1]+1; i++) {
+    SHIPIT* distances = malloc((NKDTs[1]+2) * sizeof(SHIPIT));
+    SHIPIT* distances_nd = malloc((NKDTs[1]+2) * sizeof(SHIPIT));
+    for (SHIPIT i=1; i<NKDTs[1]+1; i++) {
         scanf("%lld", &distances[i]); // array of stations
     }
     distances[0] = 0;
     distances[NKDTs[1]+1] = NKDTs[2];
 
-    long long int* TCs = malloc(4 * sizeof(long long int));
-    for (long long int i=0; i<4; i++) { // TCs[0] : Ts | TCs[1] : Cs | TCs[2] : Tf | TCs[3] : Cf
+    SHIPIT* TCs = malloc(4 * sizeof(SHIPIT));
+    for (SHIPIT i=0; i<4; i++) { // TCs[0] : Ts | TCs[1] : Cs | TCs[2] : Tf | TCs[3] : Cf
         scanf("%lld", &TCs[i]);
     }
 
-    qsort(distances, NKDTs[1]+2, sizeof(long long int), compare); // sort distances of stations
+    qsort(distances, NKDTs[1]+2, sizeof(SHIPIT), compare); // sort distances of stations
 
-    long long int uniq;
+    SHIPIT uniq;
     uniq = discard(distances, distances_nd, NKDTs[1]+2); // discard duplicate distances on stations
 
-    long long int max_distance = 0;
-    for (long long int i=1; i<uniq; i++) { // calculate longest possible distance
+    SHIPIT max_distance = 0;
+    for (SHIPIT i=1; i<uniq; i++) { // calculate longest possible distance
         if (distances_nd[i]-distances_nd[i-1] > max_distance) {
             max_distance = distances_nd[i] - distances_nd[i-1];
         }
     }
 
-    long long int optimal_capacity;
+    SHIPIT optimal_capacity;
     optimal_capacity = binary_search_answer(1, max_capacity, distances_nd, uniq, max_distance, TCs, NKDTs[3]);
 
     if (optimal_capacity == -1) {
         printf("%d\n", -1);
     } else {
-        long long int max_price = 1000000000;
-        for (long long int i=0; i<NKDTs[0]; i++) {
+        SHIPIT max_price = 1000000000;
+        for (SHIPIT i=0; i<NKDTs[0]; i++) {
             if ((cars_capacity[i] >= optimal_capacity) && (max_price >= cars_price[i])) {
                 max_price = cars_price[i];
             }
