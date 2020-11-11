331f0e68e21c310d74ce3068b7a5f2c4abf2082d
Sonqo <strats_karyp@hotmail.com>
2020-11-11 18:48:33 +0200

    Improved complexity on path finding

--- 6_Algorithms_Complexity/1/Portals/main.c
+++ 6_Algorithms_Complexity/1/Portals/main.c
@@ -1,46 +1,44 @@
 #include <stdio.h>
 #include <malloc.h>
 
-#define MIN(a, b) (((a)<(b))?(a):(b))
+#define MIN(a,b) (((a)<(b))?(a):(b))
+
+typedef long long int SHIPIT;
 
 struct edge {
-    int base, destination, weight;
+    SHIPIT base, destination, weight;
 };
 
-void make_set(int* parent, int*size, int v) {
-    parent[v] = v;
-    size[v] = 1;
+struct node {
+    SHIPIT data;
+    struct node* next;
+};
+
+void push (struct node** head, SHIPIT data) {
+    struct node* curr;
+    curr = (struct node*) malloc(sizeof(struct node));
+    curr->data = data;
+    curr->next = *head;
+    *head = curr;
 }
 
-int find_set(int* parent, int v) {
-    if (v == parent[v]) {
-        return v;
-    }
-    return parent[v] = find_set(parent, parent[v]);
-}
-
-void union_sets(int a, int b, int* parent, int* size) {
-
-    int rep_a, rep_b, t;
-
-    rep_a = find_set(parent, a);
-    rep_b = find_set(parent, b);
-    if (rep_a != rep_b) {
-        if (size[rep_a] < size[rep_b]) {
-            t = rep_a;
-            rep_a = rep_b;
-            rep_b = t;
-        }
-        parent[rep_b] = rep_a;
-        size[rep_a] += size[rep_b];
+SHIPIT pop (struct node** head) {
+    SHIPIT r;
+    if (*head != NULL) {
+        r = (*head)->data;
+        struct node* curr = *head;
+        *head = (*head) -> next;
+        free(curr);
+        return r;
+    } else {
+        return -1;
     }
 }
 
-void merge(struct edge** arr, int l, int m, int r) {
-
-    int i, j, k;
-    int n1 = m-l+1;
-    int n2 = r-m;
+void merge(struct edge** arr, SHIPIT l, SHIPIT m, SHIPIT r) {
+    SHIPIT i, j, k;
+    SHIPIT n2 = r-m;
+    SHIPIT n1 = m-l+1;
 
     struct edge* L[n1];
     struct edge* R[n2];
@@ -56,7 +54,7 @@ void merge(struct edge** arr, int l, int m, int r) {
     j = 0;
     k = l;
     while (i<n1 && j<n2) {
-        if (L[i]->weight > R[j]->weight) {
+        if (L[i]->weight <= R[j]->weight) {
             arr[k] = L[i];
             i++;
         }
@@ -77,13 +75,12 @@ void merge(struct edge** arr, int l, int m, int r) {
         j++;
         k++;
     }
-
 }
 
-void merge_sort(struct edge** arr, int l, int r) {
-
+void merge_sort(struct edge** arr, SHIPIT l, SHIPIT r) {
     if (l<r) {
-        int m = l + (r-l)/2;
+        
+        SHIPIT m = l + (r-l)/2;
 
         merge_sort(arr, l ,m);
         merge_sort(arr, m+1, r);
@@ -92,28 +89,35 @@ void merge_sort(struct edge** arr, int l, int r) {
     }
 }
 
-int main() {
+SHIPIT main() {
 
-    int NMs[2];
-    for (int i=0; i<2; i++) { // NMs[0]=N : numbers of universes | NMs[1]=M : number of portals
-        scanf("%d", &NMs[i]);
+    SHIPIT* NMs = malloc(2 * sizeof(SHIPIT));
+    for (SHIPIT i=0; i<2; i++) { // NMs[0]=N : numbers of universes | NMs[1]=M : number of portals
+        scanf("%lld", &NMs[i]);
     }
 
-    int N = NMs[0];
-    int M = NMs[1];
+    SHIPIT N = NMs[0];
+    SHIPIT M = NMs[1];
 
-    int array[N+1];
-    struct node* vertex[N+1];
-    for (int i=1; i<N+1; i++) { // read unordered array
-        scanf("%d", &array[i]);
+    struct node* ancestors[N+1];
+    SHIPIT* size = malloc((N+1) * sizeof(SHIPIT));
+    SHIPIT* array = malloc((N+1) * sizeof(SHIPIT));
+    for (SHIPIT i=1; i<N+1; i++) { // read unordered array
+        size[i] = 0;
+        scanf("%lld", &array[i]);
+
+        ancestors[i] = malloc(sizeof(struct node));
+        ancestors[i]->data = 0;
+        ancestors[i]->next = NULL;
+        push(&ancestors[i], i);
     }
 
-    struct edge* portals[M];
-    for (int i=0; i<M; i++) { // read array of points to EDGE struct
-        int acc[3];
+    struct edge** portals = malloc(M * sizeof(struct edge));
+    for (SHIPIT i=0; i<M; i++) { // read array of points to EDGE struct
+        SHIPIT acc[3];
         portals[i] = malloc(sizeof(struct edge));
-        for (int j=0; j<3; j++){
-            scanf("%d", &acc[j]);
+        for (SHIPIT j=0; j<3; j++){
+            scanf("%lld", &acc[j]);
         }
         if (acc[0] < acc[1]) {
             portals[i]->base = acc[0];
@@ -125,27 +129,50 @@ int main() {
         portals[i]->weight = acc[2];
     }
 
-    merge_sort(portals, 0, M-1); //sort pointers of edges in ascending order
-
-    int size[N+1];
-    int parent[N+1];
-
-    for (int i=1; i<N+1; i++) { // create a set for each vertex
-        make_set(parent, size, i);
+    SHIPIT* parent = malloc((N+1) * sizeof(SHIPIT)); // create parent array for union find implementation
+    for (SHIPIT i=1; i<N+1; i++) {
+        parent[i] = i;
     }
 
-    int global_min = 1000000000;
-    for (int i=0; i<M; i++) { // traverse sorted edges
-        if (find_set(parent, portals[i]->base) != find_set(parent, portals[i]->destination)) {
-            union_sets(portals[i]->base, portals[i]->destination, parent, size);
+    SHIPIT* path = malloc(((N+1) * sizeof(SHIPIT))); // immidiate ancestor array
+    SHIPIT* weight_path = malloc(((N+1) * sizeof(SHIPIT))); // weight-to-ancestor array
+    for (SHIPIT i=0; i<N+1; i++) {
+        path[i] = 0;
+        weight_path[i] = -1;
+    }
+
+    merge_sort(portals, 0, M-1); //sort pointers of edges according to their weight
+
+    SHIPIT c1, c2;
+    SHIPIT global_min = 1000000000;
+    for (SHIPIT i=M-1; i>=0; i--) { // Kruskal's algorithm
+        c1 = parent[portals[i]->base];
+        c2 = parent[portals[i]->destination];
+        if (size[c2] > size[c1]) { // connect smaller to bigger tree
+            SHIPIT t = c2;
+            c2 = c1;
+            c1 = t;
+        }
+        if (c1 != c2){
+            size[c1]++;
+            path[portals[i]->destination] = portals[i]->base;
+            global_min = MIN(global_min, portals[i]->weight);
+            weight_path[portals[i]->destination] = portals[i]->weight;
+            while (ancestors[c2]->data != 0) {
+                SHIPIT curr = pop(&ancestors[c2]);
+                parent[curr] = c1;
+                if (path[curr] == 0) {
+                    path[curr] = portals[i]->destination;
+                }
+                push(&ancestors[c1], curr);
+            }
         }
     }
 
-    int p1, p2;
-
-    int flag = 0;
-    int curr_min = 1000000000;
-//    for (int i=1; i<N+1; i++) { // check sorting sequence
+    SHIPIT p1, p2;
+    SHIPIT flag = 0;
+    SHIPIT curr_min = 1000000000;
+//    for (SHIPIT i=1; i<(N+1/2); i++) { // check sorting sequence
 //        if (array[i] != i){
 //            p1 = i;
 //            p2 = array[i];
@@ -169,7 +196,7 @@ int main() {
 //        }
 //    }
 
-//    printf("%d\n", curr_min);
+    printf("%lld\n", curr_min);
 
     return 0;
 }
