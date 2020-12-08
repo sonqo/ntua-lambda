a1994e21199ad4e3be17184533556b7ec14c8f3f
Sonqo <strats_karyp@hotmail.com>
2020-12-08 13:31:53 +0200

    Initialized Chemicals exercise

---
+++ 6_Algorithms_Complexity/2/Chemicals/cmake-build-debug/CMakeFiles/Chemicals.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Sonqo/Desktop/Ntua_Lambda/6_Algorithms_Complexity/2/Chemicals/main.c" "C:/Users/Sonqo/Desktop/Ntua_Lambda/6_Algorithms_Complexity/2/Chemicals/cmake-build-debug/CMakeFiles/Chemicals.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 6_Algorithms_Complexity/2/Chemicals/cmake-build-debug/CMakeFiles/Chemicals.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chemicals.dir/main.c.obj"
+  "Chemicals.exe"
+  "Chemicals.exe.manifest"
+  "Chemicals.pdb"
+  "libChemicals.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Chemicals.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
