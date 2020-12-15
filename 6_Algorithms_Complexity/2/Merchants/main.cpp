3ecc866af72719dae99e8530ca1f250c98a727e7
Sonqo <strats_karyp@hotmail.com>
2020-12-16 00:22:30 +0200

    Logic of Merchants added

--- 6_Algorithms_Complexity/2/Merchants/main.cpp
+++ 6_Algorithms_Complexity/2/Merchants/main.cpp
@@ -1,66 +1,9 @@
 #include <iostream>
+#include <algorithm>
 
 #define COLUMNS 1500
 
 using namespace std;
-
-void merge(int* array, int* indices, int left, int border, int right) {
-
-    int i, j, k;
-    int p2 = right-border;
-    int p1 = border-left+1;
-
-    int TempL[p1];
-    int TempIndicesL[p1];
-    int TempR[p2];
-    int TempIndicesR[p2];
-
-    for (i=0; i<p1; i++) { // spit arrays to TempL(eft) and TempR(ight)
-        TempL[i] = array[left+i];
-        TempIndicesL[i] = indices[left+i];
-    }
-    for (i=0; i<p2; i++) {
-        TempR[i] = array[border+i+1];
-        TempIndicesR[i] = indices[border+i+1];
-    }
-
-    i = 0;
-    j = 0;
-    k = left;
-    while (i<p1 && j<p2) {
-        if (TempL[i] <= TempR[j]) {
-            array[k] = TempL[i];
-            indices[k++] = TempIndicesL[i++];
-        }
-        else {
-            array[k] = TempR[j];
-            indices[k++] = TempIndicesR[j++];
-        }
-    }
-
-    while (i<p1) { // filling unsorted items
-        array[k] = TempL[i];
-        indices[k++] = TempIndicesL[i++];
-    }
-    while (j<p2) {
-        array[k] = TempR[j];
-        indices[k++] = TempIndicesR[j++];
-    }
-}
-
-void merge_sort(int* array, int* indices, int left, int right) {
-
-    if (left>=right) {
-        return;
-    }
-
-    int border = left + (right-left)/2;
-
-    merge_sort(array, indices, left, border);
-    merge_sort(array, indices, border+1, right);
-
-    merge(array, indices, left, border, right);
-}
 
 int ware2int (char input) {
     switch(input) {
@@ -77,16 +20,17 @@ int ware2int (char input) {
 
 int main() {
 
-    auto *average = (int *) malloc(4* sizeof(int));
     auto *pointers = (int *) malloc(10 * sizeof(int));
-    auto *buying_pointers = (int *) malloc(10* sizeof(int));
-
     auto **value = (int **) malloc(10 * sizeof(int *));
     auto **amount = (int **) malloc(10 * sizeof(int *));
+    auto **array = (int **) malloc(COLUMNS * sizeof(int *));
 
-    for (int i=1; i<10; i++) {
+    for (int i=0; i<COLUMNS; i++) { // allocate DP array
+        array[i] = (int *) malloc(10 * sizeof(int));
+    }
+
+    for (int i=0; i<10; i++) { // allocate value and amount arrays
         pointers[i] = 0;
-        buying_pointers[i] = 0;
         value[i] = (int *) malloc(COLUMNS * sizeof(int));
         amount[i] = (int *) malloc(COLUMNS * sizeof(int));
     }
@@ -105,36 +49,66 @@ int main() {
         scanf("%d", &value[3*curr_int+ware2int(curr[1])][pointers[3*curr_int+ware2int(curr[1])]++]);
     }
 
-    for (int i=1; i<10; i++) { // sort value of wares, keep amount indices intact
-        merge_sort(value[i], amount[i], 0, pointers[i]-1);
-    }
+    int final[3][10];
 
-    for (int i=1; i<10; i++) {
-        for (int j=0; j<pointers[i]; j++) {
-            std::cout << amount[i][j] << " " << value[i][j] << " ";
-        }
-        std::cout << std::endl;
-        if (i == 3) {
-            std::cout << std::endl;
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<10; j++) {
+            final[i][j] = 0;
         }
     }
 
-    int sum_v, min_a;
-//    while ((count < NMs[0]) && (flag != 3)) {
-        for (int i=0; i<3; i++) {
-            sum_v = value[3*i+1][buying_pointers[3*i+1]] + value[3*i+2][buying_pointers[3*i+2]] + value[3*i+3][buying_pointers[3*i+3]];
-            min_a = min({amount[3*i+1][buying_pointers[3*i+1]], amount[3*i+2][buying_pointers[3*i+2]], amount[3*i+3][buying_pointers[3*i+3]]});
-            if (min_a == 0) {
-                average[i] = -1;
-            } else {
-                average[i] = sum_v / min_a;
+    for (int m=0; m<3; m++) { // merchants
+        for (int z=1; z<4; z++) { // ware types
+            for (int i=0; i<10; i++) { // initialize first ware
+                if (pointers[3*m+z] == 0) {
+                    array[0][i] = -1;
+                } else {
+                    if (i<amount[3*m+z][0]) {
+                        array[0][i] = value[3*m+z][0];
+                    } else {
+                        array[0][i] = -1;
+                    }
+                }
             }
-            cout << average[i] << endl;
+            for (int j=1; j<pointers[3*m+z]; j++) { // traverse every other ware of the same type
+                for (int i=0; i<10; i++) {
+                    if (i < amount[3*m+z][j]) {
+                        if (array[j-1][i] > 0) {
+                            array[j][i] = min(array[j-1][i], value[3*m+z][j]);
+                        } else {
+                            array[j][i] = value[3*m+z][j];
+                        }
+                    } else if (array[j-1][i-amount[3*m+z][j]] > 0) {
+                        if (array[j-1][i] > 0) {
+                            array[j][i] = min(value[3*m+z][j] + array[j-1][i-amount[3*m+z][j]], array[j-1][i]);
+                        } else {
+                            array[j][i] = value[3*m+z][j] + array[j-1][i-amount[3*m+z][j]];
+                        }
+                    } else {
+                        array[j][i] = -1;
+                    }
+                }
+            }
+            for (int i=0; i<10; i++) {
+                if (pointers[3*m+z] == 0) {
+                    final[m][i] = -1;
+                } else {
+                    if ((array[pointers[3*m+z]-1][i] > 0) && (final[m][i] != -1)) {
+                        final[m][i] += array[pointers[3*m+z]-1][i];
+                    } else {
+                        final[m][i] = -1;
+                    }
+                }
+            }
         }
-//    }
+    }
 
-    //TODO : which merchant has the minimum average
-    //TODO : buying handling
+    for (int j=0; j<3; j++) {
+        for (int i=0; i<10; i++) {
+            cout << final[j][i] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
