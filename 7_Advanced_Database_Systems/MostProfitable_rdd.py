587dabbebbf0a1e6c0354232091eaea704d0c3dc
Sonqo <strats_karyp@hotmail.com>
2021-03-03 12:43:46 +0200

    Added explaination comments

--- 7_Advanced_Database_Systems/MostProfitable_rdd.py
+++ 7_Advanced_Database_Systems/MostProfitable_rdd.py
@@ -9,10 +9,16 @@ spark = SparkSession.builder.appName('query1_rdd').getOrCreate()
 
 sc = spark.sparkContext
 
+# read file
+# discard NaN cost, income and date, keep only 00s movies
+# create (key, values) pair where key = year
+# reduce by years keeping maximum profit
+# format output
+# write result
 res = sc.textFile('hdfs://master:9000/movies/movies.csv') \
-	.map(lambda x : split_complex(x)) \
-	.filter(lambda x : x if x[3] != "" and x[5] != '0' and x[6] != '0' and int(x[3].split('-')[0]) >= 2000 else None) \
-	.map(lambda x : (x[3].split('-')[0], x)) \
-	.reduceByKey(lambda x1, x2 : max(x1, x2, key=lambda x : ((float(x[6])-float(x[5]))/float(x[5]))*100)) \
-	.map(lambda x : (x[0], x[1][1], round(((float(x[1][6])-float(x[1][5]))/float(x[1][5])), 2))) \
-	.coalesce(1, True).saveAsTextFile('hdfs://master:9000/movies/output/query1_rdd.out')
+		.map(lambda x : split_complex(x)) \
+		.filter(lambda x : x if x[3] != "" and x[5] != '0' and x[6] != '0' and int(x[3].split('-')[0]) >= 2000 else None) \
+		.map(lambda x : (x[3].split('-')[0], x)) \
+		.reduceByKey(lambda x1, x2 : max(x1, x2, key=lambda x : ((float(x[6])-float(x[5]))/float(x[5]))*100)) \
+		.map(lambda x : (x[0], x[1][1], round(((float(x[1][6])-float(x[1][5]))/float(x[1][5])), 2))) \
+		.coalesce(1, True).saveAsTextFile('hdfs://master:9000/movies/output/query1_rdd.out')
