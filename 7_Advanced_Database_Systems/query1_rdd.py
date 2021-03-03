3a0576ff7b0d1e7d810bd06d9a44ce4044209539
Sonqo <strats_karyp@hotmail.com>
2021-03-03 09:14:55 +0200

    Added file writing

--- 7_Advanced_Database_Systems/query1_rdd.py
+++ 7_Advanced_Database_Systems/query1_rdd.py
@@ -9,19 +9,10 @@ spark = SparkSession.builder.appName('query1_rdd').getOrCreate()
 
 sc = spark.sparkContext
 
-# read file
-# discard unwanted seperators
-# discard movies with NaN gross, costs and date, keep movies after 00s
-# create (key, values) tuples where key = year
-# reduce and calculate max income for every year 
-# format output
 res = sc.textFile('hdfs://master:9000/movies/movies.csv') \
 	.map(lambda x : split_complex(x)) \
 	.filter(lambda x : x if x[3] != "" and x[5] != '0' and x[6] != '0' and int(x[3].split('-')[0]) >= 2000 else None) \
 	.map(lambda x : (x[3].split('-')[0], x)) \
 	.reduceByKey(lambda x1, x2 : max(x1, x2, key=lambda x : ((float(x[6])-float(x[5]))/float(x[5]))*100)) \
 	.map(lambda x : (x[0], x[1][1], round(((float(x[1][6])-float(x[1][5]))/float(x[1][5])), 2))) \
-	.collect()
-
-for f in res:
-	print(f)
+	.coalesce(1, True).saveAsTextFile('hdfs://master:9000/movies/output/query1_rdd.out')
