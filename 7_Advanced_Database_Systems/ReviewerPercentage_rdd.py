7018e7b27ef192ac756ee92b344c74da16480707
Sonqo <strats_karyp@hotmail.com>
2021-03-04 16:46:41 +0200

    Minor corrections on reviewer percentage query

--- 7_Advanced_Database_Systems/ReviewerPercentage_rdd.py
+++ 7_Advanced_Database_Systems/ReviewerPercentage_rdd.py
@@ -13,15 +13,15 @@ sc = spark.sparkContext
 # read file
 # create (key, value) pairs where key = user
 # calcucate for every user (number of reviews, sum of reviews)
-# calculate average review
-all = sc.textFile('hdfs://master:9000/movies/ratings.csv') \
+# calculate average reviews of user
+movies = sc.textFile('hdfs://master:9000/movies/ratings.csv') \
 	.map(lambda x : split_complex(x)) \
 	.map(lambda x : (x[0], (1, float(x[2])))) \
 	.aggregateByKey((0, 0.0), (lambda x, y : (x[0]+y[0], x[1]+y[1])), (lambda a, b : (a[0]+b[0], a[1]+b[1]))) \
 	.mapValues(lambda x : x[1]/x[0])
 
-valid = all.filter(lambda x : x[1] > 3).collect() # filter users with average review > 3
-all = all.collect()
+valid = movies.filter(lambda x : x[1] > 3).collect() # filter users with average review > 3
+movies = movies.collect()
 average = round(len(valid)/len(all), 2)
 
 # write result
