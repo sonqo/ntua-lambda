a6959a1a304c800ac9a37900f579e6b86478342b
Sonqo <strats_karyp@hotmail.com>
2021-03-06 01:39:11 +0200

    Added Most Profitable movies for every year in SQL form

---
+++ 7_Advanced_Database_Systems/SQL/MostProfitable.py
@@ -0,0 +1,27 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('query1_sql').getOrCreate()
+
+movies = spark.read.format('csv') \
+	.options(header='false', inferSchema='true') \
+	.load('hdfs://master:9000/movies/data/movies.csv')
+
+movies.registerTempTable('movies')
+
+cleaned_movies = \
+	"SELECT _c1 AS Title, YEAR(_c3) AS Date, ((_c6-_c5)/_c5)*100 AS Profit " + \
+	"FROM movies " + \
+	"WHERE YEAR(_c3) >= 2000 AND _c5 != 0 AND _c6 != 0"
+
+valid = spark.sql(cleaned_movies)
+valid.registerTempTable('cleaned_movies')
+
+sqlResult = \
+	"SELECT Date, Title, Profit " + \
+	"FROM cleaned_movies " + \
+	"WHERE (Date, Profit) IN " + \
+	"(SELECT Date, MAX(Profit) " + \
+	"FROM  cleaned_movies " + \
+	"GROUP BY Date)"
+
+res = spark.sql(sqlResult).coalesce(1).write.json('hdfs://master:9000/movies/output/query1_sql.out')
