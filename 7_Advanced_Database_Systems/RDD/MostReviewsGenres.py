a608f6f269be08938c5639b64e50f4ffd48099c6
Sonqo <strats_karyp@hotmail.com>
2021-03-09 14:11:27 +0200

    Completed BA query in RDD form

--- 7_Advanced_Database_Systems/RDD/MostReviewsGenres.py
+++ 7_Advanced_Database_Systems/RDD/MostReviewsGenres.py
@@ -11,10 +11,10 @@ sc = spark.sparkContext
 
 ratings = sc.textFile('hdfs://master:9000/movies/data/ratings.csv') \
 	.map(lambda x : split_complex(x)) \
-	.map(lambda x : (x[1], (x[0], x[2]))) # (movie, (user, rating))
+	.map(lambda x : (x[1], (x[0], x[2])))
 
 genres = sc.textFile('hdfs://master:9000/movies/data/movie_genres.csv') \
-	.map(lambda x : split_complex(x)) # (movie, genre)
+	.map(lambda x : split_complex(x))
 
 ratings_genres = ratings \
 	.join(genres) \
@@ -24,7 +24,7 @@ ratings_genres = ratings \
 temp_genre_reviewers = ratings_genres \
 	.map(lambda x : (x[0][0], (x[0][1], int(x[1])))) \
 	.reduceByKey(lambda x, y : (x[0] if x[1] >= y[1] else y[0], max(x[1], y[1]))) \
-	.map(lambda x : (x[0], (x[1][0], x[1][1]))) # (genre, (user, reviews))
+	.map(lambda x : (x[0], (x[1][0], x[1][1])))
 
 temp_ratings_genre = ratings_genres.map(lambda x : ((x[0][0], x[1]), x[0][1]))
 
@@ -35,10 +35,10 @@ genre_reviewers = temp_genre_reviewers \
 
 movies = sc.textFile('hdfs://master:9000/movies/data/movies.csv') \
 	.map(lambda x : split_complex(x)) \
-	.map(lambda x : (x[0], (x[1], x[7]))) # (movie, (title, popularity))
+	.map(lambda x : (x[0], (x[1], x[7])))
 
 ratings = ratings \
-	.map(lambda x : ((x[1][0], x[0]), x[1][1])) # ((user, movie), rating)
+	.map(lambda x : ((x[1][0], x[0]), x[1][1]))
 
 collection = genres \
 	.map(lambda x : (x[1], x[0])) \
@@ -49,10 +49,28 @@ collection = genres \
 	.join(ratings) \
 	.map(lambda x : (x[1][0][0], x[0][0], x[1][0][1], x[1][0][2], x[1][0][3], x[1][1]))
 
-### DONE
+temp_collection = collection \
+	.map(lambda x : ((x[0], x[1], x[5]), (x[2], x[3], x[4])))
 
-max_rating = collection.map(lambda x : ((x[0], x[1], x[2]), x)) \
-	.reduceByKey(lambda x, y : max(x, y, key=lambda x : x[5])).collect()
+max_rating = collection \
+	.map(lambda x : ((x[0], x[1]), x)) \
+	.reduceByKey(lambda x, y : max(x, y, key=lambda x : x[5])) \
+	.map(lambda x : ((x[0][0], x[0][1], x[1][5]), x[1][5])) \
+	.join(temp_collection) \
+	.map(lambda x : ((x[0][0], x[0][1], x[0][2]), (x[1][1][0], x[1][1][1], x[1][1][2]))) \
+	.reduceByKey(lambda x, y : max(x, y, key=lambda x : float(x[2]))) \
+	.map(lambda x : ((x[0][0], x[0][1], x[1][0]), (x[1][1], x[1][2], x[0][2])))
 
-for i in collection:
-	print(i)
+min_rating = collection \
+	.map(lambda x : ((x[0], x[1]), x)) \
+	.reduceByKey(lambda x, y : min(x, y, key=lambda x : x[5])) \
+	.map(lambda x : ((x[0][0], x[0][1], x[1][5]), x[1][5])) \
+	.join(temp_collection) \
+	.map(lambda x : ((x[0][0], x[0][1], x[0][2]), (x[1][1][0], x[1][1][1], x[1][1][2]))) \
+	.reduceByKey(lambda x, y : max(x, y, key=lambda x : float(x[2]))) \
+	.map(lambda x : ((x[0][0], x[0][1], x[1][0]), (x[1][1], x[1][2], x[0][2])))
+
+res = max_rating \
+	.join(min_rating) \
+	.map(lambda x : (x[0][0], x[0][1], x[0][2], x[1][0][0], x[1][0][1], x[1][0][2], x[1][1][0], x[1][1][1], x[1][1][2])) \
+	.coalesce(1, True).sortBy(lambda x : x[0]).saveAsTextFile('hdfs://master:9000/movies/output/query5_rdd.out')
