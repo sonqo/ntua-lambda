1382efd59ca2094ecbd6baa123c8d8fd5ad21561
Sonqo <strats_karyp@hotmail.com>
2021-03-14 21:45:46 +0200

    Corrections on BroadcastHashJoin

--- 7_Advanced_Database_Systems/broadcastJoin.py
+++ 7_Advanced_Database_Systems/broadcastJoin.py
@@ -6,8 +6,11 @@ def split_complex(x):
 	return list(csv.reader(StringIO(x), delimiter=','))[0]
 
 def broadcastJoin(pair): # mapper
+	res = []
 	if pair[0] in acc_bc.value.keys():
-		return (pair[0], (pair[1], acc_bc.value[pair[0]]))
+		for item in acc_bc.value[pair[0]]:
+			res.append((pair[0], (pair[1], item)))
+	return res
 
 spark = SparkSession.builder.appName('broadcast_join').getOrCreate()
 
@@ -31,8 +34,8 @@ acc_bc = sc.broadcast(acc)
 Left = sc.textFile('hdfs://master:9000/movies/data/ratings.csv') \
 	.map(lambda x : split_complex(x)) \
 	.map(lambda x : (x[1], (x[0], x[2], x[3]))) \
-	.map(lambda x : broadcastJoin(x)) \
-	.filter(lambda x : x is not None).collect()
+	.flatMap(lambda x : broadcastJoin(x)) \
+	.collect()
 
 for i in Left:
 	print(i)
