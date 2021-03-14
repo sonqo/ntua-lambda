5c03e7ab07028af2fd6e80cd139bd6c4fb047480
Sonqo <strats_karyp@hotmail.com>
2021-03-14 14:36:56 +0200

    Correction on data path

--- 7_Advanced_Database_Systems/RDD/ReviewerPercentage.py
+++ 7_Advanced_Database_Systems/RDD/ReviewerPercentage.py
@@ -13,7 +13,7 @@ sc = spark.sparkContext
 # create (key, value) pairs where key = user
 # calcucate for every user (number of reviews, sum of reviews)
 # calculate average reviews of user
-movies = sc.textFile('hdfs://master:9000/movies/ratings.csv') \
+movies = sc.textFile('hdfs://master:9000/movies/data/ratings.csv') \
 	.map(lambda x : split_complex(x)) \
 	.map(lambda x : (x[0], (1, float(x[2])))) \
 	.aggregateByKey((0, 0.0), (lambda x, y : (x[0]+y[0], x[1]+y[1])), (lambda a, b : (a[0]+b[0], a[1]+b[1]))) \
@@ -21,7 +21,7 @@ movies = sc.textFile('hdfs://master:9000/movies/ratings.csv') \
 
 valid = movies.filter(lambda x : x[1] > 3).collect() # filter users with average review > 3
 movies = movies.collect()
-average = round(len(valid)/len(all), 2)
+average = round(len(valid)/len(movies), 2)
 
 # write result
 res = sc.parallelize([average]) \
